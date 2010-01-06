f2949e5e337010b9d740fef07c5d6512ce7834bf
oscmax@gmail.com <oscmax@gmail.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-01-06 06:21:58 +0000

    Added missing constants for Admin manual order entry

--- 2.1/catalog/admin/includes/languages/english/create_account.php
+++ 2.1/catalog/admin/includes/languages/english/create_account.php
@@ -18,5 +18,31 @@ define('TEXT_VISITOR_CART', '<font color="#FF0000"><b> We want to get your order
 define('TEXT_RETURN_CUSTOMER', '<font color="#FF0000">Already have a profile? Please login!</font>');
 define('NAVBAR_NEW_TITLE', 'Order Process');
 define('TEXT_PRIVACY', '<font color="#FF0000"><b>Haven\'t bought from us before? Ordering is easy. Start by filling in all required fields, and then clicking the \'continue\' button. </b>');
+define('ENTRY_EMAIL_ADDRESS_ERROR_EXISTS', '&nbsp;<small><font color="#FF0000">email address already exists!</font></small>');
+define('ENTRY_EMAIL_ADDRESS_TEXT', '&nbsp;');
+define('ENTRY_STREET_ADDRESS', 'Street Address:');
+define('ENTRY_STREET_ADDRESS_ERROR', '&nbsp;<small><font color="#FF0000">min ' . ENTRY_STREET_ADDRESS_MIN_LENGTH . ' chars</font></small>');
+define('ENTRY_STREET_ADDRESS_TEXT', '&nbsp;');
+define('ENTRY_SUBURB', 'Suburb:');
+define('ENTRY_SUBURB_ERROR', '');
+define('ENTRY_SUBURB_TEXT', '');
+define('ENTRY_POST_CODE', 'Post Code:');
+define('ENTRY_POST_CODE_ERROR', '&nbsp;<small><font color="#FF0000">min ' . ENTRY_POSTCODE_MIN_LENGTH . ' chars</font></small>');
+define('ENTRY_POST_CODE_TEXT', '&nbsp;');
+define('ENTRY_CITY', 'Suburb:');
+define('ENTRY_CITY_ERROR', '&nbsp;<small><font color="#FF0000">min ' . ENTRY_CITY_MIN_LENGTH . ' chars</font></small>');
+define('ENTRY_CITY_TEXT', '&nbsp;');
+define('ENTRY_STATE', 'State/Province:');
+define('ENTRY_STATE_ERROR', '&nbsp;');
+define('ENTRY_STATE_TEXT', '&nbsp;');
+define('ENTRY_COUNTRY', 'Country:');
+define('ENTRY_COUNTRY_ERROR', '');
+define('ENTRY_COUNTRY_TEXT', '&nbsp;');
+define('ENTRY_TELEPHONE_NUMBER', 'Telephone Number:');
+define('ENTRY_TELEPHONE_NUMBER_ERROR', '&nbsp;<small><font color="#FF0000">min ' . ENTRY_TELEPHONE_MIN_LENGTH . ' chars</font></small>');
+define('ENTRY_TELEPHONE_NUMBER_TEXT', '&nbsp;');
+define('ENTRY_FAX_NUMBER', 'Fax Number:');
+define('ENTRY_FAX_NUMBER_ERROR', '');
+define('ENTRY_FAX_NUMBER_TEXT', '');
 
 ?>
