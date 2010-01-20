49ec7127aec60a6284ab88ebdbb4f35ad3c6f6eb
ejsolutions.co.uk <ejsolutions.co.uk@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-01-20 01:08:09 +0000

    Tweaks: Default selected items in Admin menu boxes.

--- 2.0/catalog/admin/includes/boxes/administrator.php
+++ 2.0/catalog/admin/includes/boxes/administrator.php
@@ -18,7 +18,8 @@ $Id: administrator.php 3 2006-05-27 04:59:07Z user $
   $contents = array();
 
   $heading[] = array('text'  => BOX_HEADING_ADMINISTRATOR,
-                     'link'  => tep_href_link(basename($PHP_SELF), tep_get_all_get_params(array('selected_box')) . 'selected_box=administrator'));
+                     'link'  => tep_href_link(FILENAME_ADMIN_MEMBERS, 'set=admin_members&selected_box=administrator'));
+//                     'link'  => tep_href_link(basename($PHP_SELF), tep_get_all_get_params(array('selected_box')) . 'selected_box=administrator'));
 
   if ($selected_box == 'administrator') {
     $contents[] = array('text'  => tep_admin_files_boxes(FILENAME_ADMIN_MEMBERS, BOX_ADMINISTRATOR_MEMBERS) .
