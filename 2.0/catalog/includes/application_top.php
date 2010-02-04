c4d01c8624488b2f04a41330d6f981aa58b2a60b
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-02-04 18:43:27 +0000

    Bugfix #301 Fix to deprecated php 5.3.  Added to application_top.php in the catalog side aswell.  Previous fix only applied to admin side.

--- 2.0/catalog/includes/application_top.php
+++ 2.0/catalog/includes/application_top.php
@@ -16,7 +16,7 @@ $Id: application_top.php 3 2006-05-27 04:59:07Z user $
 $debug = array();
 
 // set the level of error reporting
-  error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+  error_reporting(E_ALL & ~E_NOTICE & ~'E_DEPRECATED');
 
 // check support for register_globals
   if (function_exists('ini_get') && (ini_get('register_globals') == false) && (PHP_VERSION < 4.3) ) {
