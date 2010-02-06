a965ea714f225dfbabe72099252d0a861f28137c
michael.oscmax <michael.oscmax@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-02-06 02:12:42 +0000

    Bugfix #353 - header link to osCMax.com incorrectly uses ssl Added current oscmax version to image 'alt' and 'title' tags in header Changed several links to external sites in admin to use target _blank

--- 2.0/catalog/admin/includes/header.php
+++ 2.0/catalog/admin/includes/header.php
@@ -16,7 +16,7 @@ $Id: header.php 3 2006-05-27 04:59:07Z user $
 ?>
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
   <tr>
-    <td><?php echo '<a href="http://www.oscmax.com">' . tep_image(DIR_WS_IMAGES . 'oscmax-logo.png', 'osCMax v2.0', '85', '80') . '</a>'; ?></td>
+    <td><?php echo '<a href="http://www.oscmax.com">' . tep_image(DIR_WS_IMAGES . 'oscmax-logo.png', PROJECT_VERSION, '85', '80') . '</a>'; ?></td>
     <td align="right"><?php echo '<a href="http://www.oscmax.com/" target="_blank">' . tep_image(DIR_WS_IMAGES . 'header_support.gif', HEADER_TITLE_SUPPORT_SITE, '50', '50') . '</a>&nbsp;&nbsp;<a href="' . tep_catalog_href_link() . '">' . tep_image(DIR_WS_IMAGES . 'header_checkout.gif', HEADER_TITLE_ONLINE_CATALOG, '53', '50') . '</a>&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'header_administration.gif', HEADER_TITLE_ADMINISTRATION, '50', '50') . '</a>'; ?>&nbsp;&nbsp;</td>
   </tr>
   <tr class="headerBar">
@@ -31,7 +31,7 @@ $Id: header.php 3 2006-05-27 04:59:07Z user $
     echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '" class="headerLink">' . HEADER_TITLE_TOP . '</a>';
   }
     ?></td>
-    <td class="headerBarContent" align="right"><?php echo '&nbsp;&nbsp;<a href="https://www.oscmax.com/" target="_blank" class="headerLink">osCMax.com</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://shop.oscmax.com" class="headerLink">' . HEADER_TITLE_OSCDOX . '</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a href="' . tep_catalog_href_link() . '" class="headerLink">' . HEADER_TITLE_ONLINE_CATALOG . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '" class="headerLink">' . HEADER_TITLE_ADMINISTRATION . '</a>'; ?>&nbsp;&nbsp;</td>
+    <td class="headerBarContent" align="right"><?php echo '&nbsp;&nbsp;<a href="http://www.oscmax.com/" target="_blank" class="headerLink">osCMax.com</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://shop.oscmax.com" target="_blank" class="headerLink">' . HEADER_TITLE_OSCDOX . '</a>&nbsp;&nbsp;|&nbsp;&nbsp; <a href="' . tep_catalog_href_link() . '" target="_blank" class="headerLink">' . HEADER_TITLE_ONLINE_CATALOG . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '" class="headerLink">' . HEADER_TITLE_ADMINISTRATION . '</a>'; ?>&nbsp;&nbsp;</td>
 <?php /* EOF: MOD - Admin Security */ ?>
 
   </tr>
