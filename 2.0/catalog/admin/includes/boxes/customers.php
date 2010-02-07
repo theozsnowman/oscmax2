19cdcb64663b3a384d3105e985e33912c1666feb
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-02-07 18:24:31 +0000

    Bugfix #323 v2
    r328 was missing a " after the menuBoxContentLink
    Also if the var is passed we have to pass it in all cases - this updates all the boxes to use the extra target variable.

--- 2.0/catalog/admin/includes/boxes/customers.php
+++ 2.0/catalog/admin/includes/boxes/customers.php
@@ -25,11 +25,11 @@ $Id: customers.php 3 2006-05-27 04:59:07Z user $
 //BOF: MOD - Admin Sercurty
 //old-$contents[] = array('text'  => '<a href="' . tep_href_link(FILENAME_CUSTOMERS, '', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_CUSTOMERS_CUSTOMERS . '</a><br>' .
 //old-                               '<a href="' . tep_href_link(FILENAME_ORDERS, '', 'NONSSL') . '" class="menuBoxContentLink">' . BOX_CUSTOMERS_ORDERS . '</a>');
-		$contents[] = array('text'  => tep_admin_files_boxes(FILENAME_CUSTOMERS, BOX_CUSTOMERS_CUSTOMERS) .
-                                   tep_admin_files_boxes(FILENAME_CUSTOMERS_GROUPS, BOX_CUSTOMERS_GROUPS) .
-                                   tep_admin_files_boxes(FILENAME_CREATE_ACCOUNT, BOX_MANUAL_ORDER_CREATE_ACCOUNT) .
-                                   tep_admin_files_boxes(FILENAME_CREATE_ORDER, BOX_MANUAL_ORDER_CREATE_ORDER) .
-                                   tep_admin_files_boxes(FILENAME_ORDERS, BOX_CUSTOMERS_ORDERS));
+		$contents[] = array('text'  => tep_admin_files_boxes(FILENAME_CUSTOMERS, BOX_CUSTOMERS_CUSTOMERS, TOP) .
+                                   tep_admin_files_boxes(FILENAME_CUSTOMERS_GROUPS, BOX_CUSTOMERS_GROUPS, TOP) .
+                                   tep_admin_files_boxes(FILENAME_CREATE_ACCOUNT, BOX_MANUAL_ORDER_CREATE_ACCOUNT, TOP) .
+                                   tep_admin_files_boxes(FILENAME_CREATE_ORDER, BOX_MANUAL_ORDER_CREATE_ORDER, TOP) .
+                                   tep_admin_files_boxes(FILENAME_ORDERS, BOX_CUSTOMERS_ORDERS, TOP));
 //EOF: MOD - Admin Security
   }
 
