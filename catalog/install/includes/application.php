8469f86747a4f565b67239a129ad3cbadc300d3b
michael.oscmax <michael.oscmax@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-02-10 02:13:18 +0000

    Parse error in installer fixed.

--- catalog/install/includes/application.php
+++ catalog/install/includes/application.php
@@ -11,7 +11,7 @@ $Id: application.php 3 2006-05-27 04:59:07Z user $
 */
 
 // Set the level of error reporting
-  error_reporting(E_ALL & ~E_NOTICE & ~'E_DEPRECATED';
+  error_reporting(E_ALL & ~E_NOTICE & ~'E_DEPRECATED');
 
   require('includes/functions/compatibility.php');
   require('includes/functions/general.php');
