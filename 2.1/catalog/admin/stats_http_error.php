9fa7c444229e68a9db1abc5bd9b1342a6f9cc68a
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-04-02 09:23:29 +0000

    Tweak: Added column_left code for stats_http_error.php

--- 2.1/catalog/admin/stats_http_error.php
+++ 2.1/catalog/admin/stats_http_error.php
@@ -40,7 +40,13 @@ $Id: stats_http_error.php 3 2009-11-19 15:09:00 user pgm
 
 <!-- body //-->
 <table border="0" width="100%" cellspacing="2" cellpadding="2">
-  <tr><!-- body_text //-->
+  <tr>
+    <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+        </table></td>
+<!-- body_text //-->
     <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
