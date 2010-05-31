f1f89552fcb79740cb6733f681b1908618a8b98a
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-05-31 12:08:08 +0000

    Bug Fix: added product_status check to query as products were being shown in the upcoming_products.php module even if the status was set to false.  Hence showing a product not found page.

--- 2.1/catalog/includes/modules/upcoming_products.php
+++ 2.1/catalog/includes/modules/upcoming_products.php
@@ -10,7 +10,7 @@ $Id: upcoming_products.php 3 2006-05-27 04:59:07Z user $
   Released under the GNU General Public License
 */
 
-  $expected_query = tep_db_query("select p.products_id, pd.products_name, products_date_available as date_expected from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where to_days(products_date_available) >= to_days(now()) and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' order by " . EXPECTED_PRODUCTS_FIELD . " " . EXPECTED_PRODUCTS_SORT . " limit " . MAX_DISPLAY_UPCOMING_PRODUCTS);
+  $expected_query = tep_db_query("select p.products_id, pd.products_name, products_date_available as date_expected from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where to_days(products_date_available) >= to_days(now()) and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' and p.products_status = '1' order by " . EXPECTED_PRODUCTS_FIELD . " " . EXPECTED_PRODUCTS_SORT . " limit " . MAX_DISPLAY_UPCOMING_PRODUCTS);
   if (tep_db_num_rows($expected_query) > 0) {
 ?>
 <!-- upcoming_products //-->
