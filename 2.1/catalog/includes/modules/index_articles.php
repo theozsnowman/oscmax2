9e1e595bb269edf27b241b06f467076184eec96f
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-06-16 22:41:58 +0000

    Bug Fix: Articles for contentBox on homepage had code outside the conditional statement causing layout problems when no articles are displayed.

--- 2.1/catalog/includes/modules/index_articles.php
+++ 2.1/catalog/includes/modules/index_articles.php
@@ -13,12 +13,13 @@ $Id: index_articles.php 3 2010-05-03 PGM
 
   $articles_query = tep_db_query("select a.articles_id, a.articles_date_added, ad.articles_name, ad.articles_description from " . TABLE_ARTICLES . " a, " . TABLE_ARTICLES_DESCRIPTION . " ad where (a.articles_date_available IS NULL or to_days(a.articles_date_available) <= to_days(now())) and a.articles_status = '1' and a.articles_index_status = '1' and ad.language_id = '" . (int)$languages_id . "' and a.articles_id = ad.articles_id order by a.articles_date_added desc, ad.articles_name");
 
-$info_box_contents = array();
-$info_box_contents[] = array('align' => 'left', 'text' => '<a href="' . tep_href_link(FILENAME_ARTICLES) . '" class="headerNavigation">' . TABLE_HEADING_DEFAULT_ARTICLES . '</a>');
-  new infoBoxHeading($info_box_contents, true, true, tep_href_link(FILENAME_ARTICLES));
-
-
   if (tep_db_num_rows($articles_query) > 0) {
+
+
+	$info_box_contents = array();
+	$info_box_contents[] = array('align' => 'left', 'text' => '<a href="' . tep_href_link(FILENAME_ARTICLES) . '" class="headerNavigation">' . TABLE_HEADING_DEFAULT_ARTICLES . '</a>');
+	new infoBoxHeading($info_box_contents, true, true, tep_href_link(FILENAME_ARTICLES));
+
 	while ($index_articles = tep_db_fetch_array($articles_query)) {
 ?>
 <!-- index_articles //-->
@@ -38,6 +39,6 @@ $info_box_contents[] = array('align' => 'left', 'text' => '<a href="' . tep_href
           	<td><?php echo tep_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
           </tr>
 <!-- index_articles_eof //-->
-<?php } // end while ?>
+	<?php } // end while ?>
 
 <?php } ?> 
