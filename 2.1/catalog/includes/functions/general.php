5ecac93f949fc7473fb6dbc671617da54f2bf16a
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-06-20 13:16:44 +0000

    Tweak: Switches small + and - for column sorts to icons.
    To do:
    - Add control for both up and down arrow showing on/off state.

--- 2.1/catalog/includes/functions/general.php
+++ 2.1/catalog/includes/functions/general.php
@@ -986,7 +986,7 @@ $Id: general.php 14 2006-07-28 17:42:07Z user $
 
     if ($sortby) {
       $sort_prefix = '<a href="' . tep_href_link(basename($PHP_SELF), tep_get_all_get_params(array('page', 'info', 'sort')) . 'page=1&sort=' . $colnum . ($sortby == $colnum . 'a' ? 'd' : 'a')) . '" title="' . tep_output_string(TEXT_SORT_PRODUCTS . ($sortby == $colnum . 'd' || substr($sortby, 0, 1) != $colnum ? TEXT_ASCENDINGLY : TEXT_DESCENDINGLY) . TEXT_BY . $heading) . '" class="productListing-heading">' ;
-      $sort_suffix = (substr($sortby, 0, 1) == $colnum ? (substr($sortby, 1, 1) == 'a' ? '+' : '-') : '') . '</a>';
+      $sort_suffix = '&nbsp;' . (substr($sortby, 0, 1) == $colnum ? (substr($sortby, 1, 1) == 'a' ? tep_image(DIR_WS_ICONS . 'down_off.png') : tep_image(DIR_WS_ICONS . 'up_off.png')) : '' ) . '</a>';
     }
 
     return $sort_prefix . $heading . $sort_suffix;
