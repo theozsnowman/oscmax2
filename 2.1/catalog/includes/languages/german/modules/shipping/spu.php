cf838c1cc87bf070484d2ae06c6674b2c74747e7
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-07-02 18:05:17 +0000

    Feature: Store Pickup Shipping Module

---
+++ 2.1/catalog/includes/languages/german/modules/shipping/spu.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  $Id: spu.php,v 1.4 2002/11/10 14:29:56 mattice Exp $
+  CONTRIB is Store Pickup Shipping Module (http://www.oscommerce.com/community/contributions,164)
+  Based upon flat.php / spu.php by M. Halvorsen (http://www.arachnia-web.com)
+
+  Made to work with latest check-out procedure by Matthijs (Mattice)
+     >> e-mail:    mattice@xs4all.nl 
+     >> site:      http://www.matthijs.org
+ 
+  TO TRANSLATE IN GERMAN !!
+  
+osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2003 osCommerce
+
+  Released under the GNU General Public License
+  
+  ATRADUIRE
+*/
+
+define('MODULE_SHIPPING_SPU_TEXT_TITLE', 'Abholung in unserem Laden. MO-FR 9:30/11:30-14:00/18:00');
+define('MODULE_SHIPPING_SPU_TEXT_DESCRIPTION', 'Abholung in unserem Laden');
+define('MODULE_SHIPPING_SPU_TEXT_WAY', 'Abholung in unserem Laden<br> in Via Borghetto 12, in 6512, Giubiasco (TI)');
+?>
