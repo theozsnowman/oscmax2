5a50f3a96143119db496a7d02c5fe5c3ceb74ae0
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-08-20 19:26:56 +0000

    Tweak: premade_comments.php - Added code to show error messages - Added right hand column
    To do:
    - Language files
    - Switch input buttons for graphic images to match osCmax design

--- 2.1/catalog/admin/premade_comments.php
+++ 2.1/catalog/admin/premade_comments.php
@@ -29,33 +29,60 @@
 <!-- body //-->
 <table border="0" width="100%" cellspacing="2" cellpadding="2">
   <tr>
-    <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
-<!-- left_navigation //-->
-<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
-<!-- left_navigation_eof //-->
-    </table></td>
-<!-- body_text //-->
-    <td width="100%" valign="top"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
-      <tr>
-        <td class="pageHeading">Premade Order Comments</td>
-      </tr>
-      <tr>
-        <td class="main">
-								<p><br>
-								</p>
-							</td>
-      </tr>
-      <tr>
-        <td>
-        
-        <?php
-        
-        if ($_POST[submit]) {
+    <td width="<?php echo BOX_WIDTH; ?>" valign="top">
+      <table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
+        <!-- left_navigation //-->
+        <?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+        <!-- left_navigation_eof //-->
+      </table>
+    </td>
+    <!-- body_text //-->
+    <td width="100%" valign="top">
+      <table width="100%" border="0" cellspacing="0" cellpadding="2">
+        <tr>
+          <td class="pageHeading" width="75%">Premade Order Comments</td>
+          <td width="25%">&nbsp;</td>
+        </tr>
+        <tr class="dataTableHeadingRow">
+          <td class="dataTableHeadingContent">&nbsp;</td>
+          <td class="infoBoxHeading">Premade Order Comments</td>
+        </tr>
+        <tr>
+          <td><?php echo tep_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
+          <td rowspan="4" class="infoBoxContent" valign="top">
+          <?php
+            if ($_POST[submit]) {
+		  ?>
+			  You have saved your change to your Premade comment.
+          <?php	
+			} elseif ($_POST[delete]) {
+		  ?>
+			  You have deleted a Premade comment.
+          <?php	
+			} elseif ($_POST[add]) {
+		  ?>
+			  You have added a new Premade comment.
+          <?php	
+			} else {
+		  ?>
+              Please either <b>Select</b> an existing Premade Comment from the drop down list or <b>Add new</b> comment.
+		  <?php
+		    } // end if
+		  ?>
+          </td>
+        </tr>
+        <tr>
+          <td>
+          <?php
+          if ($_POST[submit]) {
 				$namecheck = mysql_fetch_array(mysql_query("SELECT * FROM orders_premade_comments WHERE title='$_POST[title]'"));
 				$err = "";
 				
-				if (!$_POST[title] or !$_POST[text]) {
-					$err .= "Required fields missing.<br>";
+				if (!$_POST[title]) {
+					$err .= "The required field <b>Title</b> is missing.<br>";
+				}
+				if (!$_POST[text]) {
+				    $err .= "The required field <b>Text</b> is missing.<br>";
 				}
 				if ($namecheck[title] and ($_POST[title] !== $_POST[old_title])) {
 					$err .= "Category already exists.<br>";
@@ -72,8 +99,11 @@
 			}
 			elseif ($_POST[add]) {
 				$err = "";
-				if (!$_POST[title] or !$_POST[text]) {
-					$err .= "Required fields missing.<br>";
+				if (!$_POST[title]) {
+					$err .= "The required field <b>Title</b> is missing.<br>";
+				}
+				if (!$_POST[text]) {
+				    $err .= "The required field <b>Text</b> is missing.<br>";
 				}
 				if (!$err) {
 					mysql_query("INSERT INTO orders_premade_comments (title, text) VALUES ('$_POST[title]', '$_POST[text]')");
@@ -85,60 +115,110 @@
 			}
 			?>
 			
-			<table width="100%" border="0" cellspacing=0 cellpadding=0>
-<form action="" method="post">
-<input type="hidden" name="a" value="premade">
-<input type="hidden" name="p_id" value="<?php echo $_POST[p_id]; ?>">
-<tr><td>
-<table border="0" cellspacing=0 cellpadding=0><tr><td>
-<select name="p_id">
+			<table width="100%" border="0" cellspacing="0" cellpadding="0">
+			  <form action="" method="post">
+	            <input type="hidden" name="a" value="premade">
+    	        <input type="hidden" name="p_id" value="<?php echo $_POST[p_id]; ?>">
+			  <tr>
+                <td>
+				  <table border="0" cellspacing="3" cellpadding="0">
+                    <tr>
+                      <td class="main" width="120px"><b>Premade Replies:</b></td>
+                      <td><?php echo tep_draw_separator('pixel_trans.gif', '5', '5'); ?></td>
+                      <td>
+						<select name="p_id">
 
-<?php
-	$premades = mysql_query("SELECT * FROM orders_premade_comments order by title");
-while ($premade = mysql_fetch_array($premades)) {
-	$selected = ($premade[id] == $p_id) ? " SELECTED": "";
-	?> <option value="<?php echo $premade[id]; ?>"<?php echo $selected; ?>><?php echo $premade[title]; ?></option>
-<?php
-}
-?> </select></td><td>&nbsp;</td><td><input type="submit" name="select" value="Select" class="inputsubmit">
-<input class="inputsubmit" type="submit" name="delete" value="Delete"> <input type="submit" name="submit_new" value="Add New" class="inputsubmit"></td></tr></table>
-<p>
-
-<table width="100%" border="0" cellspacing=1 cellpadding=2 class="TableMsg">
-<tr><td class="main" width="120">&nbsp;<b>Premade Replies:</b></td><td>&nbsp;</td></tr>
-<?php
-if (!$_POST[p_id]) { $_POST[submit_new] = true; }
-
-if ($_POST[select]) {
-	$premade = mysql_fetch_array(mysql_query("SELECT * FROM orders_premade_comments WHERE id='".$_POST['p_id']."'"));
-    ?> 
-    <input type="hidden" name="old_title" value="<?php echo $premade[title]; ?>">
-<tr><td class="main"><b>Title:</b></td><td class="mainTableAlt"><input type="text" name="title" value="<?php echo$premade[title]; ?>" size="60"></td></tr>
-<tr><td class="main" valign="top"><b>Text:</b></td><td class="mainTableAlt"><textarea name="text" cols="58" rows="10"><?php echo $premade[text]; ?></textarea></td></tr>
-</table>
-<p>
-<input class="inputsubmit" type="submit" name="submit" value="Save Changes">
-<?php
-}
-elseif ($_POST[submit_new] or $_POST[submit] or $_POST[add] or $_POST[delete]) {
-    ?> <tr><td class="main"><b>Title:</b></td><td class="mainTableAlt"><input type="text" name="title" size="60"></td></tr>
-<tr><td class="main" valign="top"><b>Text:</b></td><td class="mainTableAlt"><textarea name="text" cols="58" rows="10"></textarea></td></tr>
-</table>
-<p>
-<input class="inputsubmit" type="submit" name="add" value="Create Reply">
-<?php
-}
-?>
-</td></tr></form></table>
-
-        
-        </td>
-      </tr>
-						<tr>
-        <td class="main"><br>
-							</td>
-      </tr>
-					</table></td>
+						<?php
+                        $premades = mysql_query("SELECT * FROM orders_premade_comments order by title");
+                          while ($premade = mysql_fetch_array($premades)) {
+                              $selected = ($premade[id] == $p_id) ? " SELECTED": "";
+                        ?>
+                          <option value="<?php echo $premade[id]; ?>"<?php echo $selected; ?>><?php echo $premade[title]; ?></option>
+						<?php
+						  } // end while
+						?>
+                        </select>
+                      </td>
+                      <td><?php echo tep_draw_separator('pixel_trans.gif', '5', '5'); ?></td>
+                      <td><input type="submit" name="select" value="Select" class="inputsubmit"></td>
+                      <td><?php echo tep_draw_separator('pixel_trans.gif', '5', '5'); ?></td>
+                      <td><input type="submit" name="submit_new" value="Add New" class="inputsubmit"></td>
+                    </tr>
+                    <tr>
+                      <td><?php echo tep_draw_separator('pixel_trans.gif', '10', '10'); ?></td>
+                    </tr>
+                  </table>
+                  
+                  <?php
+				  if ($err <> '') {
+				  ?>
+                  <table width="100%">
+                    <tr>
+                      <td class="messageStackError"><?php echo $err; ?></td>
+                    </tr>
+				  </table>
+                  <?php
+                  } // end if
+				  ?>
+                  
+                  <table border="0" cellspacing="3" cellpadding="0" class="TableMsg">
+				    
+				<?php
+                if (!$_POST[p_id]) { $_POST[submit_new] = true; }
+                if ($_POST[select]) {
+                  $premade = mysql_fetch_array(mysql_query("SELECT * FROM orders_premade_comments WHERE id='".$_POST['p_id']."'"));
+                ?> 
+                	<tr>
+                      <td class="main" colspan="2"><b>Edit Premade Comment:</b></td>
+                    </tr>
+                     <input type="hidden" name="old_title" value="<?php echo $premade[title]; ?>">
+				    <tr>
+                      <td class="main" width="120"><b>Title:</b></td>
+                      <td class="mainTableAlt"><input type="text" name="title" value="<?php echo $premade[title]; ?>" size="60"></td>
+                    </tr>
+				    <tr>
+                      <td class="main" valign="top"><b>Text:</b></td>
+                      <td class="mainTableAlt"><textarea name="text" cols="58" rows="10"><?php echo $premade[text]; ?></textarea></td>
+                    </tr>
+                    <tr>
+                      <td colspan="2" align="right"><input class="inputsubmit" type="submit" name="delete" value="Delete">
+                      	  <?php echo tep_draw_separator('pixel_trans.gif', '5', '5'); ?>
+				  		  <input class="inputsubmit" type="submit" name="submit" value="Save Changes"></td>
+                    </tr>
+				  </table>
+				<?php
+				} elseif ($_POST[submit_new] or $_POST[submit] or $_POST[add] or $_POST[delete]) { // Create New
+				?>
+                    <tr>
+                      <td class="main" colspan="2"><b>Create New Premade Comment:</b></td>
+                    </tr>
+                    <tr>
+                      <td class="main" width="120"><b>Title:</b></td>
+                      <td class="mainTableAlt"><input type="text" name="title" size="60"></td>
+                    </tr>
+					<tr>
+                      <td class="main" valign="top"><b>Text:</b></td>
+                      <td class="mainTableAlt"><textarea name="text" cols="58" rows="10"></textarea></td>
+                    </tr>
+                    <tr>
+                      <td colspan="2" align="right"><input class="inputsubmit" type="submit" name="add" value="Create Reply"></td>
+                    </tr>
+				  </table>
+				  
+				<?php
+				} // end if
+				?>
+				  </td>
+                </tr>
+              </form>
+            </table>
+          </td>
+        </tr>
+	    <tr>
+          <td class="main"><br></td>
+        </tr>
+	  </table>
+    </td>
     <!-- body_text_eof //-->
   </tr>
 </table>
@@ -147,7 +227,6 @@ elseif ($_POST[submit_new] or $_POST[submit] or $_POST[add] or $_POST[delete]) {
 <!-- footer //-->
 <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
 <!-- footer_eof //-->
-<br>
 </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
