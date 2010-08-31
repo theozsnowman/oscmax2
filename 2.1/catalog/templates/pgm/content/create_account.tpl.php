1aa1e9c5f53866b5224a55d8e234a96a5caeb5d7
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-08-31 20:06:19 +0000

    Tweak: Ajax country W3C fix
    - Missing </span> for ENTRY_STATE_TEXT

--- 2.1/catalog/templates/pgm/content/create_account.tpl.php
+++ 2.1/catalog/templates/pgm/content/create_account.tpl.php
@@ -175,7 +175,7 @@
 		  echo tep_draw_input_field('state');
 		}
 // EOF: MOD - Country-State Selector
-    if (tep_not_null(ENTRY_STATE_TEXT)) echo '&nbsp;<span class="inputRequirement">' . ENTRY_STATE_TEXT;
+    if (tep_not_null(ENTRY_STATE_TEXT)) echo '&nbsp;<span class="inputRequirement">' . ENTRY_STATE_TEXT . '</span>';
 ?>
                 </td>
               </tr>
