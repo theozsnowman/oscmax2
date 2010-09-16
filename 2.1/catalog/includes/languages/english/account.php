195e320e9237c6bf2a798c1e81cf4f1387cbc225
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-09-16 21:38:26 +0000

    Tweak: Redesign Customer Account page

--- 2.1/catalog/includes/languages/english/account.php
+++ 2.1/catalog/includes/languages/english/account.php
@@ -18,14 +18,15 @@ define('OVERVIEW_SHOW_ALL_ORDERS', '(show all orders)');
 define('OVERVIEW_PREVIOUS_ORDERS', 'Previous Orders');
 
 define('MY_ACCOUNT_TITLE', 'My Account');
-define('MY_ACCOUNT_INFORMATION', 'View or change my account information.');
-define('MY_ACCOUNT_ADDRESS_BOOK', 'View or change entries in my address book.');
-define('MY_ACCOUNT_PASSWORD', 'Change my account password.');
+define('MY_ACCOUNT_INFORMATION', '<b>Account Information</b><br><i>View or change my account information</i>');
+define('MY_ACCOUNT_ADDRESS_BOOK', '<b>Address Book</b><br><i>View or update my address details</i>');
+define('MY_ACCOUNT_PASSWORD', '<b>Password</b><br><i>Change my account password</i>');
 
 define('MY_ORDERS_TITLE', 'My Orders');
-define('MY_ORDERS_VIEW', 'View the orders I have made.');
+define('MY_ORDERS_VIEW', '<b>Previous Orders</b><br><i>View my previous orders</i>');
+define('MY_WISH_LIST', '<b>Wish List</b><br><i>View my wish list</i>');
 
 define('EMAIL_NOTIFICATIONS_TITLE', 'E-Mail Notifications');
-define('EMAIL_NOTIFICATIONS_NEWSLETTERS', 'Subscribe or unsubscribe from newsletters.');
-define('EMAIL_NOTIFICATIONS_PRODUCTS', 'View or change my product notification list.');
+define('EMAIL_NOTIFICATIONS_NEWSLETTERS', '<b>Newsletters</b><br><i>Manage my newsletter subscriptions</i>');
+define('EMAIL_NOTIFICATIONS_PRODUCTS', '<b>Product Notifications</b><br><i>View or update my product notifications</i>');
 ?>
