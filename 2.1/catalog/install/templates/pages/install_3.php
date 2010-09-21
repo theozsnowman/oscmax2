d8f103d4f29feeee07f2cf402e55e878ccd959f4
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-09-21 18:43:28 +0000

    Bug Fix: #396 Installer checks file permissions prior to trying to change the admin name. - If it can't change it then it displays a warning and a link to the wiki security page. - Changed link to go to the v2.1 namespace version of the wiki.

--- 2.1/catalog/install/templates/pages/install_3.php
+++ 2.1/catalog/install/templates/pages/install_3.php
@@ -9,6 +9,16 @@ $Id: install_3.php 3 2006-05-27 04:59:07Z user $
 
   Released under the GNU General Public License
 */
+
+$dir_fs_document_root = $HTTP_POST_VARS['DIR_FS_DOCUMENT_ROOT'];
+  if ((substr($dir_fs_document_root, -1) != '\\') && (substr($dir_fs_document_root, -1) != '/')) {
+    if (strrpos($dir_fs_document_root, '\\') !== false) {
+      $dir_fs_document_root .= '\\';
+    } else {
+      $dir_fs_document_root .= '/';
+    }
+  }
+
 ?>
 
 <div class="mainBlock">
@@ -67,11 +77,18 @@ $Id: install_3.php 3 2006-05-27 04:59:07Z user $
         <td class="inputField"><?php echo 'Administrator Password<br />' . osc_draw_password_field('CFG_ADMINISTRATOR_PASSWORD', null, 'class="text"'); ?></td>
         <td class="inputDescription">The password to use for the administrator account.</td>
       </tr>
+
+<?php if (is_writable($dir_fs_document_root) && is_writable($dir_fs_document_root . 'admin')) { ?>
       <tr>
         <td class="inputField"><?php echo 'Admin Folder Name<br />' . osc_draw_input_field('CFG_ADMIN_FOLDER', 'admin', 'class="text"'); ?></td>
-        <td class="inputDescription">The name of folder in which the admin files should be kept.  It is <b>recommended that you change this</b> from the default setting of <b>admin</b> to improve your site's security. If you want to read more about security please <a href="http://wiki.oscdox.com/setting_up_security" target="_blank">read the wiki</a>.</td>
+        <td class="inputDescription">The name of folder in which the admin files should be kept.  It is <b>recommended that you change this</b> from the default setting of <b>admin</b> to improve your site's security. If you want to read more about security please <a href="http://wiki.oscdox.com/v2.1/setting_up_security" target="_blank">read the wiki</a>.</td>
       </tr>
-
+<?php } else { ?>
+      <tr>
+        <td class="inputField"><?php echo 'Admin Folder Name<br /><img src="images/failed.gif" align="left" hspace="5" vspace="5" border="0">';?></td>
+        <td class="inputDescription">We have been unable to obtain sufficient file permissions to allow you to change the name of your <b>admin/</b> folder.  You should rename this folder to improve your store security.  For instructions on how to manually do this once you have corrected your server settings please <a href="http://wiki.oscdox.com/v2.1/setting_up_security" target="_blank">read the wiki</a>.</td>
+      </tr>
+<?php } ?>
     </table>
 
     <p align="right"><input type="image" src="images/button_continue.gif" border="0" alt="Continue" id="inputButton" />&nbsp;&nbsp;<a href="index.php"><img src="images/button_cancel.gif" border="0" alt="Cancel" /></a></p>
