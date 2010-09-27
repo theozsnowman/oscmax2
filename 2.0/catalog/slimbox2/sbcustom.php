f1f9d9481982e9006d58cf5ad05e0904bccd1779
ejsolutions.co.uk <ejsolutions.co.uk@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-09-27 19:07:15 +0000

    Bug Fix: Slimbox2 customisation: Very small tweak for IE (Firefox ignores an additional comma).

--- 2.0/catalog/slimbox2/sbcustom.php
+++ 2.0/catalog/slimbox2/sbcustom.php
@@ -11,7 +11,7 @@ jQuery(function($) {
 	resizeDuration: <?php echo SLIMBOX_RESIZE ;?>,
 	counterText: <?php echo SLIMBOX_COUNTER ;?>,
 	captionAnimationDuration: <?php echo SLIMBOX_CAPTION ;?>, 
-	resizeEasing: <?php echo SLIMBOX_EASING ;?>, 
+	resizeEasing: <?php echo SLIMBOX_EASING ;?> 
 	/* End of custom options */
 	}, null, function(el) {
 		return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
