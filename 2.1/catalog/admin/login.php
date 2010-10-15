d20afe67d18cdd826fe35c72c0a344dc592291ed
sales@driedflowersrus.com <sales@driedflowersrus.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-10-15 22:54:08 +0000

    modified sql inserts to mysql5 strict compliant

--- 2.1/catalog/admin/login.php
+++ 2.1/catalog/admin/login.php
@@ -21,7 +21,7 @@ $Id: login.php 3 2006-05-27 04:59:07Z user $
     if (!tep_db_num_rows($check_admin_query)) {
 
 //Added by PGM
-tep_db_query("insert into " . TABLE_ADMIN_LOG . " values ('', '" . $username . "', '" . $_SERVER['REMOTE_ADDR'] . "', 'Wrong Username', '" . date('F j, Y, g:i a') . "')");
+    tep_db_query("insert into " . TABLE_ADMIN_LOG . " values (NULL, '" . $username . "', '" . $_SERVER['REMOTE_ADDR'] . "', 'Wrong Username', '" . date('F j, Y, g:i a') . "')");
 
       $_GET['login'] = 'fail';
     } else {
@@ -30,7 +30,7 @@ tep_db_query("insert into " . TABLE_ADMIN_LOG . " values ('', '" . $username . "
       if (!tep_validate_password($password, $check_admin['login_password'])) {
 
 //Added by PGM
-tep_db_query("insert into " . TABLE_ADMIN_LOG . " values ('', '" . $username . "', '" . $_SERVER['REMOTE_ADDR'] . "', 'Wrong Password', '" . date('F j, Y, g:i a') . "')");
+    tep_db_query("insert into " . TABLE_ADMIN_LOG . " values (NULL, '" . $username . "', '" . $_SERVER['REMOTE_ADDR'] . "', 'Wrong Password', '" . date('F j, Y, g:i a') . "')");
 
         $_GET['login'] = 'fail';
       } else {
@@ -53,7 +53,7 @@ tep_db_query("insert into " . TABLE_ADMIN_LOG . " values ('', '" . $username . "
         tep_db_query("update " . TABLE_ADMIN . " set admin_logdate = now(), admin_lognum = admin_lognum+1 where admin_id = '" . $login_id . "'");
 
 //Added by PGM
-tep_db_query("insert into " . TABLE_ADMIN_LOG . " values ('', '" . $login_username . "', '" . $_SERVER['REMOTE_ADDR'] . "', 'Logged In', '" . date('F j, Y, g:i a') . "')");
+    tep_db_query("insert into " . TABLE_ADMIN_LOG . " values (NULL, '" . $login_username . "', '" . $_SERVER['REMOTE_ADDR'] . "', 'Logged In', '" . date('F j, Y, g:i a') . "')");
 
 // There is no more default ADMIN - so don't need to check for DEFAULT user
 //      if (($login_lognum == 0) || !($login_logdate) || ($login_email_address == 'admin@localhost') || ($login_modified == '0000-00-00 00:00:00')) {
