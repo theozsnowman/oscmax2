0efc2a7c562a4d5c8dba0c8d7949800d834771c6
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-12-14 09:07:12 +0000

    Bug Fix: #521 - Typo

--- 2.1/catalog/includes/languages/english/create_account_success.php
+++ 2.1/catalog/includes/languages/english/create_account_success.php
@@ -13,5 +13,5 @@ $Id: create_account_success.php 3 2006-05-27 04:59:07Z user $
 define('NAVBAR_TITLE_1', 'Create an Account');
 define('NAVBAR_TITLE_2', 'Success');
 define('HEADING_TITLE', 'Your Account Has Been Created!');
-define('TEXT_ACCOUNT_CREATED', 'Congratulations! Your new account has been successfully created! You can now take advantage of member priviledges to enhance your online shopping experience with us. If you have <small><b>ANY</b></small> questions about the operation of this online shop, please email the <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">store owner</a>.<br><br>A confirmation has been sent to the provided email address. If you have not received it within the hour, please <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">contact us</a>.');
+define('TEXT_ACCOUNT_CREATED', 'Congratulations! Your new account has been successfully created! You can now take advantage of member privileges to enhance your online shopping experience with us. If you have <small><b>ANY</b></small> questions about the operation of this online shop, please email the <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">store owner</a>.<br><br>A confirmation has been sent to the provided email address. If you have not received it within the hour, please <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">contact us</a>.');
 ?>
