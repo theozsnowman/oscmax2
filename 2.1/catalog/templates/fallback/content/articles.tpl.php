06cc1b44b66f42dd22a234e7af0803d8f9407d3a
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-12-21 21:37:53 +0000

    Bug Fix: #529 - Part 12 - articles.tpl.php fixes

--- 2.1/catalog/templates/fallback/content/articles.tpl.php
+++ 2.1/catalog/templates/fallback/content/articles.tpl.php
@@ -172,7 +172,7 @@
                 <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
               </tr>
               <?php } ?>
-              <?php if (tep_not_null($authors_description)) { ?>
+              <?php if (tep_not_null(isset($authors_description))) { ?>
               <tr>
                 <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
               </tr>
@@ -180,7 +180,7 @@
                 <td class="main" colspan="2" valign="top"><?php echo $authors_description; ?></td>
               <tr>
                 <?php } ?>
-                <?php if (tep_not_null($authors_url)) { ?>
+                <?php if (tep_not_null(isset($authors_url))) { ?>
               <tr>
                 <td colspan="2"><?php echo tep_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
               </tr>
@@ -248,20 +248,6 @@
       <tr>
         </table></td>
         </tr>
-        <?php
-  if (($articles_all_split->number_of_rows > 0) && ((ARTICLE_PREV_NEXT_BAR_LOCATION == 'bottom') || (ARTICLE_PREV_NEXT_BAR_LOCATION == 'both'))) {
-?>
-        <tr>
-          <td><table border="0" width="100%" cellspacing="0" cellpadding="2" class="filterbox">
-              <tr>
-                <td class="smallText"><?php echo $articles_all_split->display_count(TEXT_DISPLAY_NUMBER_OF_ARTICLES); ?></td>
-                <td align="right" class="smallText"><?php echo TEXT_RESULT_PAGE . ' ' . $articles_all_split->display_links(MAX_DISPLAY_PAGE_LINKS, tep_get_all_get_params(array('page', 'info', 'x', 'y'))); ?></td>
-              </tr>
-            </table></td>
-        </tr>
-        <?php
-  }
-?>
       </table></td>
     <?php } ?>
     <!-- body_text_eof //-->
