76c21682c11e52591ab40e7918d194263cf61ab2
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2010-12-30 11:14:17 +0000

    Bug Fix: #552 - Welcome Message module displaying useless buttons.
    To do:
    
    - Add right hand column to this module.

--- 2.1/catalog/admin/information_list.php
+++ 2.1/catalog/admin/information_list.php
@@ -18,11 +18,11 @@
   <td>
     <table border="0" width="100%"  cellpadding="2" cellspacing="0" bgcolor="#ffffff">
       <tr class="dataTableHeadingRow">
-	    <td align="center" class="dataTableHeadingContent"><?php echo ID_INFORMATION;?></td>
+	    <td align="center" class="dataTableHeadingContent" width="50"><?php echo ID_INFORMATION;?></td>
 	    <td align="left" class="dataTableHeadingContent"><?php echo ENTRY_TITLE;?></td>
-	    <td align="left" class="dataTableHeadingContent"><?php echo ENTRY_PARENT_PAGE;?></td>
-	    <td align="center" class="dataTableHeadingContent"><?php if ($_GET['gID'] == '1') { echo PUBLIC_INFORMATION; } else { echo PUBLIC_INFORMATION_2; }?></td>
-	    <td align="center" class="dataTableHeadingContent"><?php echo ENTRY_SORT_ORDER; ?></td>
+	    <td align="left" class="dataTableHeadingContent"><?php if ($_GET['gID'] == '1') { echo ENTRY_PARENT_PAGE; } else { echo '&nbsp;'; }?></td>
+	    <td align="center" class="dataTableHeadingContent"><?php if ($_GET['gID'] == '1') { echo PUBLIC_INFORMATION; } else { echo '&nbsp;'; }?></td>
+	    <td align="center" class="dataTableHeadingContent"><?php if ($_GET['gID'] == '1') { echo ENTRY_SORT_ORDER; } else { echo '&nbsp;'; }?></td>
 	    <td align="center" class="dataTableHeadingContent" colspan=2><?php echo ACTION_INFORMATION;?></td>
       </tr>
 <?php
@@ -34,22 +34,23 @@ if (sizeof($data) > 0) {
         <td align="center" class="dataTableContent"><?php echo $val['information_id']; ?></td>
         <td width="40%" class="dataTableContent"><?php echo $val['information_title'];?></td>
         <td class="dataTableContent"><?php echo ((!empty($val['parent_id'])) ? $val['parent_id'] : null);?></td>
-        <td nowrap  align="center" class="dataTableContent">
+        <td nowrap align="center" class="dataTableContent">
 <?php
-		if ($val['visible'] == 1) {
+        if ($_GET['gID'] == '1') { 
+		  if ($val['visible'] == 1) {
 			echo tep_image(DIR_WS_ICONS . 'icon_status_green.gif', IMAGE_ICON_STATUS_GREEN, 10, 10) . '&nbsp;&nbsp;';
 			echo ( (!strstr($info_group['locked'], 'visible')) ? '<a href="' . tep_href_link(FILENAME_INFORMATION_MANAGER, "gID=$gID&amp;information_action=Visible&amp;information_id=$val[information_id]&amp;visible=0") . '">' : null);
 			echo tep_image(DIR_WS_ICONS . 'icon_status_red_light.gif', DEACTIVATION_ID_INFORMATION . " $val[information_title]", 10, 10);
 			echo ( (!strstr($info_group['locked'], 'visible')) ? '</a>' : null);
-		}
-		else {
+		  } else {
 			echo ( (!strstr($info_group['locked'], 'visible')) ? '<a href="' . tep_href_link(FILENAME_INFORMATION_MANAGER, "gID=$gID&amp;information_action=Visible&amp;information_id=$val[information_id]&amp;visible=1") . '">' : null);
 			echo tep_image(DIR_WS_ICONS . 'icon_status_green_light.gif', ACTIVATION_ID_INFORMATION . " $val[information_title]", 10, 10);
 			echo ( (!strstr($info_group['locked'], 'visible')) ? '</a>' : null);
 			echo '&nbsp;&nbsp;' . tep_image(DIR_WS_ICONS . 'icon_status_red.gif', IMAGE_ICON_STATUS_RED, 10, 10);
-		}
+		  } // end if ($val['visible'] == 1)
+		} // end if ($_GET['gID'] == '1')
 ?></td>
-	    <td width="10%" align="center" class="dataTableContent"><?php echo $val['sort_order'];?></td>
+	    <td width="10%" align="center" class="dataTableContent"><?php if ($_GET['gID'] == '1') { echo $val['sort_order']; } else { echo '&nbsp;'; } ?></td>
         <td align=center class="dataTableContent">
 		<?php echo '<a href="' . tep_href_link(FILENAME_INFORMATION_MANAGER, "gID=$gID&amp;information_action=Edit&amp;information_id=$val[information_id]", 'NONSSL') . '">' . tep_image(DIR_WS_ICONS . 'page_white_edit.png', EDIT_ID_INFORMATION . " $val[information_title]") . '</a>'; ?></td>
 		<?php echo ( empty($info_group['locked']) ? '<td align=center class="dataTableContent"><a href="' . tep_href_link(FILENAME_INFORMATION_MANAGER, "gID=$gID&amp;information_action=Delete&amp;information_id=$val[information_id]", 'NONSSL') . '">' . tep_image(DIR_WS_ICONS . 'delete.gif', DELETE_ID_INFORMATION . " $val[information_title]") . '</a></td>' : null); ?>
