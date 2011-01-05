60f708d89daaceec7dab2aff133a636f1ca9f5f2
michael.oscmax@gmail.com <michael.oscmax@gmail.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-01-05 21:50:11 +0000

    Header Block update Part 6 - missed one file in main catalog directory. Updating header comment block to be consistent throughout project

--- 2.1/catalog/wishlist.php
+++ 2.1/catalog/wishlist.php
@@ -4,7 +4,7 @@ $Id$
   osCmax e-Commerce
   http://www.oscmax.com
 
-  Copyright 2006 osCMax2005 osCMax, 2002 osCommerce
+  Copyright 2000 - 2011 osCmax
 
   Released under the GNU General Public License
 */
