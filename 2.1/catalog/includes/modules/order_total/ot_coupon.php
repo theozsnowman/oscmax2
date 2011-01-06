978b31888581cb8ba09d715b7b7a5fc79a4b43fb
michael.oscmax@gmail.com <michael.oscmax@gmail.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-01-06 01:10:39 +0000

    Header Block update Part 14 - Order total modules

--- 2.1/catalog/includes/modules/order_total/ot_coupon.php
+++ 2.1/catalog/includes/modules/order_total/ot_coupon.php
@@ -1,13 +1,13 @@
 <?php
 /*
-$Id: ot_coupon.php 14 2006-07-28 17:42:07Z user $
+$Id$
 
-  osCMax Power E-Commerce
-  http://oscdox.com
+  osCmax e-Commerce
+  http://www.oscmax.com
 
-  Copyright 2006 osCMax2005 osCMax, 2002 osCommerce
+  Copyright 2000 - 2011 osCmax
 
-Released under the GNU General Public License
+  Released under the GNU General Public License
 */
 
 class ot_coupon {
--- 2.1/catalog/includes/modules/order_total/ot_loyalty_discount.php
+++ 2.1/catalog/includes/modules/order_total/ot_loyalty_discount.php
@@ -1,13 +1,11 @@
 <?php
 /*
-ot_loyalty_discount.php
-$Id: ot_loyalty_discount.php 14 2006-07-28 17:42:07Z user $
+$Id$
 
+  osCmax e-Commerce
+  http://www.oscmax.com
 
-  osCMax Power E-Commerce
-  http://oscdox.com
-
-  Copyright 2006 osCMax2005 osCMax, 2002 osCommerce
+  Copyright 2000 - 2011 osCmax
 
   Released under the GNU General Plic License
 */
