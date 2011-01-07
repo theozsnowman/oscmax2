c059628abdb2da8cdb78c8688ca1737494fd9b0f
michael.oscmax@gmail.com <michael.oscmax@gmail.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-01-07 00:33:21 +0000

    Header Block update Part 38 - admin graphs

--- 2.1/catalog/admin/includes/graphs/banner_monthly.php
+++ 2.1/catalog/admin/includes/graphs/banner_monthly.php
@@ -1,9 +1,12 @@
 <?php
 /*
-$Id: banner_monthly.php 3 2006-05-27 04:59:07Z user $
-  osCMax Power E-Commerce
-  http://oscdox.com
-  Copyright 2006 osCMax2005 osCMax, 2002 osCommerce
+$Id$
+
+  osCmax e-Commerce
+  http://www.oscmax.com
+
+  Copyright 2000 - 2011 osCmax
+
   Released under the GNU General Public License
 */
   include(DIR_WS_CLASSES . 'phplot.php');
