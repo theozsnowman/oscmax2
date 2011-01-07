b3f7ccb9e6d9b059330df63413d448c772da220e
michael.oscmax@gmail.com <michael.oscmax@gmail.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-01-07 01:01:15 +0000

    Header Block update Part 41 - admin boxes

--- 2.1/catalog/admin/includes/boxes/customers.php
+++ 2.1/catalog/admin/includes/boxes/customers.php
@@ -1,11 +1,11 @@
 <?php
 /*
-$Id: customers.php 2009-11-14 19:38:07Z user $
+$Id$
 
-  osCMax Power E-Commerce
-  http://oscdox.com
+  osCmax e-Commerce
+  http://www.oscmax.com
 
-  Copyright 2009 osCMax, 2006 osCMax ,2005 osCMax, 2002 osCommerce
+  Copyright 2000 - 2011 osCmax
 
   Released under the GNU General Public License
 */
