8f5a76b9faf3ca4d23dc5823dff874abd2a91e45
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-01-10 22:40:53 +0000

    Tweak: Small change to OPC checkout/comments.php to valign the row to top and add small spacer.

--- 2.1/catalog/includes/checkout/comments.php
+++ 2.1/catalog/includes/checkout/comments.php
@@ -13,7 +13,8 @@ $Id$
 
 <table border="0" width="100%" cellspacing="0" cellpadding="2">
  <tr>
-  <td width="50%"><?php echo tep_draw_textarea_field('comments', '60', '5', $comments); ?></td>
-  <td width="50%"><div class="finalProducts"></div><br><div style="float:right" class="orderTotals"><?php echo (MODULE_ORDER_TOTAL_INSTALLED ? '<table cellpadding="2" cellspacing="0" border="0">' . $order_total_modules->output() . '</table>' : '');?></div></td>
+  <td width="49%" valign="top"><?php echo tep_draw_textarea_field('comments', '60', '6', $comments); ?></td>
+  <td width="2%"></td>
+  <td width="49%" valign="top"><div class="finalProducts"></div><br><div style="float:right" class="orderTotals"><?php echo (MODULE_ORDER_TOTAL_INSTALLED ? '<table cellpadding="2" cellspacing="0" border="0">' . $order_total_modules->output() . '</table>' : '');?></div></td>
  </tr>
 </table>
