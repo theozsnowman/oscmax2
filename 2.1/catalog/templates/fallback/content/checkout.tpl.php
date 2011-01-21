2661b210d97c6224ac10c1f494fc219a44d767dd
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-01-21 19:33:13 +0000

    Bug FIx: #551 - Tidies up styling for <noscript> message for checkout.tpl.php
    To do:
    - Make multi-lingual

--- 2.1/catalog/templates/fallback/content/checkout.tpl.php
+++ 2.1/catalog/templates/fallback/content/checkout.tpl.php
@@ -12,8 +12,9 @@ $Id$
 ?>
 <!-- body_text //-->
 <noscript>
- <p>Please follow the instructions for your web browser:<br /><br />Internet Explorer</p>
- <ol>
+ <table width="100%"><tr><td class="messageStackError">We have detected that you have JavaScript disabled.</td></tr></table>
+ <p style="font-family: Arial, Helvetica, sans-serif; font-size:12px; font-weight:bold">Please follow the instructions for your web browser in order to complete your order:<br /><br />Internet Explorer</p>
+ <ol style="font-family: Arial, Helvetica, sans-serif; font-size:12px;">
   <li>On the&nbsp;<strong>Tools</strong>&nbsp;menu, click&nbsp;<strong>Internet Options</strong>, and then click the&nbsp;<strong>Security</strong>&nbsp;tab.</li>
   <li>Click the&nbsp;<strong>Internet</strong>&nbsp;zone.</li>
   <li>If you do not have to customize your Internet security settings, click&nbsp;<strong>Default Level</strong>. Then do step 4<blockquote>If you have to customize your Internet security settings, follow these steps:<br />
@@ -21,11 +22,18 @@ $Id$
 	b. In the&nbsp;<strong>Security Settings &ndash; Internet Zone</strong>&nbsp;dialog box, click&nbsp;<strong>Enable</strong>&nbsp;for&nbsp;<strong>Active Scripting</strong>&nbsp;in the&nbsp;<strong>Scripting</strong>section.</blockquote></li>
   <li>Click the&nbsp;<strong>Back</strong>&nbsp;button to return to the previous page, and then click the&nbsp;<strong>Refresh</strong>&nbsp;button to run scripts.</li>
  </ol>
- <p><br />Firefox</p>
- <ol>
+ <p style="font-family: Arial, Helvetica, sans-serif; font-size:12px; font-weight:bold"><br />Firefox</p>
+ <ol style="font-family: Arial, Helvetica, sans-serif; font-size:12px;">
   <li>On the&nbsp;<strong>Tools</strong>&nbsp;menu, click&nbsp;<strong>Options</strong>.</li>
   <li>On the&nbsp;<strong>Content</strong>&nbsp;tab, click to select the&nbsp;<strong>Enable JavaScript</strong>&nbsp;check box.</li>
   <li>Click the&nbsp;<strong>Go back one page</strong>&nbsp;button to return to the previous page, and then click the&nbsp;<strong>Reload current page</strong>&nbsp;button to run scripts.</li>
+ </ol>
+ <p style="font-family: Arial, Helvetica, sans-serif; font-size:12px; font-weight:bold"><br />Safari</p>
+ <ol style="font-family: Arial, Helvetica, sans-serif; font-size:12px;">
+  <li>Click the <strong>Safari</strong> menu.</li>
+  <li>Select <strong>Preferences</strong>.</li>
+  <li>Click the <strong>Security</strong> tab.</li>
+  <li>Select the 'Enable JavaScript' checkbox.</li>
  </ol>
  <p>&nbsp;</p>
 </noscript>
