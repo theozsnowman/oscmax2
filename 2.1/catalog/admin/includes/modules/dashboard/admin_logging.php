9213c786686af4b9b83e97a13d6f7ae7f4686f38
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-03-02 12:02:32 +0000

    Bug Fix: #713 - Small W3C fix for link back to admin setting

--- 2.1/catalog/admin/includes/modules/dashboard/admin_logging.php
+++ 2.1/catalog/admin/includes/modules/dashboard/admin_logging.php
@@ -54,7 +54,7 @@ $Id$
 			  $cfg_group_query = tep_db_query("select configuration_title, configuration_description, configuration_group_id from " . TABLE_CONFIGURATION . " where configuration_id = '" . (int)$config_id . "'");
               $cfg_group = tep_db_fetch_array($cfg_group_query);
 			  ?>
-                <td align="right"><?php echo '<a href="' . tep_href_link(FILENAME_CONFIGURATION, 'gID=' . $cfg_group['configuration_group_id']) . '&cID=' . (int)$config_id . '">' . tep_image(DIR_WS_ICONS . 'page_white_edit.png', ''); ?></a></td>
+                <td align="right"><?php echo '<a href="' . tep_href_link(FILENAME_CONFIGURATION, 'gID=' . $cfg_group['configuration_group_id']) . '&amp;cID=' . (int)$config_id . '">' . tep_image(DIR_WS_ICONS . 'page_white_edit.png', ''); ?></a></td>
 				<td class="dataTableContent"><?php echo '<span title="' . $cfg_group['configuration_title'] . '|' . strip_tags($cfg_group['configuration_description'], '<p><br><b>') . '">' . $admin_logging['type']; ?></span></td>
               <?php } ?>
 			</tr>
