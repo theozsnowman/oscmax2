70716a76c2af089467b9f5cc9b43dca5f5564e44
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-03-03 23:46:51 +0000

    Tweak: Widens the product name field to 60.

--- 2.1/catalog/admin/categories.php
+++ 2.1/catalog/admin/categories.php
@@ -1500,7 +1500,7 @@ function updateMSRPNet() {
             <td class="main"><?php if ($i == 0) echo TEXT_PRODUCTS_NAME; ?></td>
 <?php /* LINE CHANGED: MS2 update 501112 - Added: stripslashes(...) */ ?>
             <td class="main">
-            <?php echo tep_image(DIR_WS_CATALOG_LANGUAGES . $languages[$i]['directory'] . '/images/' . $languages[$i]['image'], $languages[$i]['name']) . '&nbsp;' . tep_draw_input_field('products_name[' . $languages[$i]['id'] . ']', (isset($products_name[$languages[$i]['id']]) ? stripslashes($products_name[$languages[$i]['id']]) : tep_get_products_name($pInfo->products_id, $languages[$i]['id']))) .
+            <?php echo tep_image(DIR_WS_CATALOG_LANGUAGES . $languages[$i]['directory'] . '/images/' . $languages[$i]['image'], $languages[$i]['name']) . '&nbsp;' . tep_draw_input_field('products_name[' . $languages[$i]['id'] . ']', (isset($products_name[$languages[$i]['id']]) ? stripslashes($products_name[$languages[$i]['id']]) : tep_get_products_name($pInfo->products_id, $languages[$i]['id'])), ' size="60"') .
             tep_draw_separator('pixel_trans.gif', '15', '12') ; ?>
             <?php if ($i == 0) {
                         echo TEXT_PRODUCTS_URL . TEXT_PRODUCTS_URL_WITHOUT_HTTP . tep_draw_separator('pixel_trans.gif', '30', '12');
