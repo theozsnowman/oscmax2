45fb4a85bd453ab59363f73ecb3d8bfd9ec37b4e
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-03-16 23:10:57 +0000

    Bug Fix: #757 - SEO Language conversions for special characters
    - updates headers to match osCmax ones.

--- 2.1/catalog/includes/modules/ultimate_seo_urls5/includes/character_conversion/espanol.php
+++ 2.1/catalog/includes/modules/ultimate_seo_urls5/includes/character_conversion/espanol.php
@@ -1,4 +1,15 @@
 <?php
+/*
+$Id: espanol.php 956 2011-01-05 19:10:37Z michael.oscmax@gmail.com $
+
+  osCmax e-Commerce
+  http://www.oscmax.com
+
+  Copyright 2000 - 2011 osCmax
+
+  Released under the GNU General Public License
+*/
+
  /**
  *
  * ULTIMATE Seo Urls 5
