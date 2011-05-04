dbc2612ca681e06b8b82e6ae85efe756f34e87d7
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-05-04 20:58:13 +0000

    Bug Fix: #865 - Slideshow ALT typo fix

--- 2.1/catalog/admin/slideshow.php
+++ 2.1/catalog/admin/slideshow.php
@@ -129,7 +129,7 @@ $Id: slideshow.php 3 2010-03-31 user pgm
   if (empty($action)) {
 ?>
                   <tr>
-                    <td colspan="6" align="right"><?php echo '<a href="' . tep_href_link(FILENAME_SLIDESHOW, 'page=' . $_GET['page'] . '&amp;action=new') . '">' . tep_image_button('button_insert.gif', IMAGE_NEW_TAX_RATE) . '</a>'; ?></td>
+                    <td colspan="6" align="right"><?php echo '<a href="' . tep_href_link(FILENAME_SLIDESHOW, 'page=' . $_GET['page'] . '&amp;action=new') . '">' . tep_image_button('button_insert.gif', IMAGE_INSERT) . '</a>'; ?></td>
                   </tr>
 <?php
   }
