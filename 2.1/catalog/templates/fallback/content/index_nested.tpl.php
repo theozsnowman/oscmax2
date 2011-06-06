2075771b1618ef0a4d816159ac3ef208789b8712
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-06-06 19:28:44 +0000

    Bug Fix: #898 - Moves nested_index into using the same code as the rest of the site.
    - Also removes the underline a:hover in nested and main listing.

--- 2.1/catalog/templates/fallback/content/index_nested.tpl.php
+++ 2.1/catalog/templates/fallback/content/index_nested.tpl.php
@@ -45,10 +45,11 @@ $Id$
       </tr>
       <?php } // end if (ALLOW_CATEGORY_DESCRIPTIONS == 'true') ?>
       <tr>
-        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
           <tr>
-            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
               <tr>
+                <td>
 <?php
 //    if (isset($cPath) && strpos('_', $cPath)) {
 // // check to see if there are deeper categories within the current category
@@ -88,21 +89,57 @@ $Id$
 
     $number_of_categories = tep_db_num_rows($categories_query);
 
-    $rows = 0;
+    $row = 0;
+	$column = 0;
+	
     while ($categories = tep_db_fetch_array($categories_query)) {
-    $rows++;
-    $cPath_new = tep_get_path($categories['categories_id'], $categories['parent_id'], $categories['grand_parent_id']);
-      $width = (int)(100 / MAX_DISPLAY_CATEGORIES_PER_ROW) . '%';
-      echo '                <td align="center" class="smallText" width="' . $width . '" valign="top"><a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new) . '">' . tep_image(DIR_WS_IMAGES . CATEGORY_IMAGES_DIR . $categories['categories_image'], $categories['categories_name'], SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT) . '<br>' . $categories['categories_name'] . '</a></td>' . "\n";
-      if ((($rows / MAX_DISPLAY_CATEGORIES_PER_ROW) == floor($rows / MAX_DISPLAY_CATEGORIES_PER_ROW)) && ($rows != $number_of_categories)) {
-        echo '              </tr>' . "\n";
-        echo '              <tr>' . "\n";
-      }
-    }
+	  	
+	  $cPath_new = tep_get_path($categories['categories_id'], $categories['parent_id'], $categories['grand_parent_id']);	
+		
+	  $list_box_contents[$row][$column] = array('align' => 'center',
+                                                'params' => 'class="categoryListing-data"',
+                                                'text'  => '<a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new) . '">' . tep_image(DIR_WS_IMAGES . CATEGORY_IMAGES_DIR . $categories['categories_image'], $categories['categories_name'], SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT) . '<br>' . $categories['categories_name'] . '</a>');
+												
+	  $column ++;
+	  
+	  //Adds a spacer column between the product column - checks if it is the last column - if it is leave it out.
+	  if ($column != PRODUCT_LIST_NUM_COLUMNS + 2) { // Adds 2 to the column count to allow for spacers
+	  $list_box_contents[$row][$column] = array('align' => 'center',
+                                                'params' => 'class="categoryListing-data-spacer"',
+                                                'text'  => tep_draw_separator('pixel_trans.gif', '10', '10'));
+	  }
+	  $column ++;
+	  
+	  if ($column >= PRODUCT_LIST_NUM_COLUMNS + (PRODUCT_LIST_NUM_COLUMNS-1)) {
+        	  $row ++;
+			  // Add a row spacer between the product rows
+			  $list_box_contents[$row][0] = array('align' => 'center',
+                                                'params' => 'class="categoryListing-data-spacer"',
+                                                'text'  => tep_draw_separator('pixel_trans.gif', '10', '10'));
+			  
+			  $row ++;
+        	  $column = 0;
+	  }	
+	}
+		
+ //     $rows++;
+ //     $cPath_new = tep_get_path($categories['categories_id'], $categories['parent_id'], $categories['grand_parent_id']);
+ //     $width = (int)(100 / MAX_DISPLAY_CATEGORIES_PER_ROW) . '%';
+      
+//	  echo '                <td align="center" class="productListing-data" width="' . $width . '" valign="top"><a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new) . '">' . tep_image(DIR_WS_IMAGES . CATEGORY_IMAGES_DIR . $categories['categories_image'], $categories['categories_name'], SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT) . '<br>' . $categories['categories_name'] . '</a></td><td>' . tep_draw_separator('pixel_trans.gif', '10', '10') . $rows/ MAX_DISPLAY_CATEGORIES_PER_ROW . ':' . floor($rows / MAX_DISPLAY_CATEGORIES_PER_ROW) . '</td>' . "\n";
+  //    if ((($rows / MAX_DISPLAY_CATEGORIES_PER_ROW) == floor($rows / MAX_DISPLAY_CATEGORIES_PER_ROW)) && ($rows != $number_of_categories)) {
+    //    echo '              </tr>' . "\n";//
+//		echo '              <tr><td>' . tep_draw_separator('pixel_trans.gif', '10', '10') . '</td></tr>';
+  //      echo '              <tr>' . "\n";
+  //    }
+ //   }
+	
+	new productListingBox($list_box_contents);
 
 // needed for the new products module shown below
     $new_products_category_id = $current_category_id;
 ?>
+                </td>
               </tr>
             </table></td>
           </tr>
