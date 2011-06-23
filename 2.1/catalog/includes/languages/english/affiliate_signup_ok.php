34ed8ed2db476dd8a866fe1199cd6202fa57c234
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-06-23 08:46:54 +0000

    Bug Fix: #920 - Affiliate contact us link fix

--- 2.1/catalog/includes/languages/english/affiliate_signup_ok.php
+++ 2.1/catalog/includes/languages/english/affiliate_signup_ok.php
@@ -12,5 +12,5 @@ $Id$
 
 define('NAVBAR_TITLE', 'Affiliate Signup');
 define('HEADING_TITLE', 'Congratulations!');
-define('TEXT_ACCOUNT_CREATED', 'Congratulations! Your new Affiliate account application has been submitted! You will shortly receive an email containing important information regarding your Affiliate Account, including you affiliate login details. If you have not received it within the hour, please <a href="' . tep_href_link(FILENAME_AFFILIATE_CONTACT) . '">contact us</a>.<br><br>If you have <small><b>ANY</b></small> questions about the affiliate program, please <a href="' . tep_href_link(FILENAME_AFFILIATE_CONTACT) . '">contact us</a>.');
+define('TEXT_ACCOUNT_CREATED', 'Congratulations! Your new Affiliate account application has been submitted! You will shortly receive an email containing important information regarding your Affiliate Account, including you affiliate login details. If you have not received it within the hour, please <a href="' . tep_href_link(FILENAME_CONTACT_US, 'source=affiliate&amp;enquiry=' . TEXT_AFFILIATE_CONTACT_TEXT) . '">contact us</a>.<br><br>If you have <small><b>ANY</b></small> questions about the affiliate program, please <a href="' . tep_href_link(FILENAME_CONTACT_US, 'source=affiliate&amp;enquiry=' . TEXT_AFFILIATE_CONTACT_TEXT) . '">contact us</a>.');
 ?>
