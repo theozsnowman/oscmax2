d3e20231f91481d8f80b5ad9e90e22108b69c944
michael.oscmax@gmail.com <michael.oscmax@gmail.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-07-21 01:43:31 +0000

    Added osCmax Quantum Gateway payment module.

---
+++ 2.1/catalog/includes/languages/espanol/quantumqgwdbe.php
@@ -0,0 +1,34 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+
+  eProcessingNetwork.php was developed for eProcessingNetwork
+
+  http://www.quantumgateway.com
+
+  by
+
+  Andres Roca - CDG Commerce
+  andresr@cdgcommerce.com
+*/
+
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_TITLE', 'QuantumGateway');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_DESCRIPTION', 'Tarjeta de Cr&eacute;dito para pruebas:<br><br>N&uacute;mero:: 4111111111111111<br>Caducidad: Cualquiera');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_TYPE', 'Tipo de Tarjeta:');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_CREDIT_CARD_OWNER', 'Titular de la tarjeta:');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_CREDIT_CARD_NUMBER', 'N&uacute;mero de la Tarjeta:');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_CREDIT_CARD_EXPIRES', 'Fecha de Caducidad:');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_NOT_CVV', 'Active si no puede ecribir el c&oacute;digo CVV:');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_REASON_NOT_CVV', '&iquest;Por qu&eacute; no puede escribir el c&oacute;digo CVV?:');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_JS_CC_OWNER', '* El campo titular de la tarjeta debe contener al menos ' . CC_OWNER_MIN_LENGTH . ' caracteres.\n');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_JS_CC_NUMBER', '* El n&uacute;mero de la tarjeta debe ser de al menos ' . CC_NUMBER_MIN_LENGTH . ' caracteres.\n');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_ERROR_MESSAGE', 'Ha ocurrido un error procesando su tarjeta. Por favor intente de nuevo.');
+  define('MODULE_PAYMENT_QUANTUMQGWDBE_TEXT_ERROR', 'Error en Tarjeta de Cr&eacute;dito!');
+?>
