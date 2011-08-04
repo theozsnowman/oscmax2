c51a40d20a81bea56aebbf17bb2e4a1ce09617dd
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-08-04 16:00:12 +0000

    Bug FIx: #950 - Small Typo in German files - fix by Vortex32.

--- 2.1/catalog/includes/languages/german/shopping_cart.php
+++ 2.1/catalog/includes/languages/german/shopping_cart.php
@@ -25,7 +25,7 @@ define('SUB_TITLE_SUB_TOTAL', 'Zwischensumme:');
 define('SUB_TITLE_TOTAL', 'Gesamtsumme:');
 
 define('OUT_OF_STOCK_CANT_CHECKOUT', 'Die mit ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' markierten Produkte, sind leider nicht in ausreichender Menge lagernd.<br>Bitte reduzieren Sie Ihre Bestellmenge bei den mit ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' gekennzeichneten Produkte');
-define('OUT_OF_STOCK_CAN_CHECKOUT', 'Die mit ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' markierten Produkte, sind leider nicht in ausreichender Menge lagernd.<br>Sie kÃ¶nnen die Bestellung dennoch durchführen und die verfügbare Menge für eine prompte Lieferung bei Bestellungsabschluss anpassen.');
+define('OUT_OF_STOCK_CAN_CHECKOUT', 'Die mit ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' markierten Produkte, sind leider nicht in ausreichender Menge lagernd.<br>Sie können die Bestellung dennoch durchführen und die verfügbare Menge für eine prompte Lieferung bei Bestellungsabschluss anpassen.');
 define('TEXT_ALTERNATIVE_CHECKOUT_METHODS', '- ODER -');
 define('CLEAR_CART', 'Sind Sie sicher, dass Sie Ihren gesamten Warenkorb leeren mÃ¶chten?');
 
