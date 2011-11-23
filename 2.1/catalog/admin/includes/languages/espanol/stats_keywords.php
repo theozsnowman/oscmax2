1f874fc7442ed0f5d27afbc35eceb87c3800983d
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-11-23 22:40:08 +0000

    Bug Fix: #1016 - Spanish Language translation for stats_keywords.php by dalogar - many thanks!

---
+++ 2.1/catalog/admin/includes/languages/espanol/stats_keywords.php
@@ -0,0 +1,26 @@
+<?php
+
+define('HEADING_TITLE', 'Palabras clave de búsquedas: Estadísticas y reemplazos');
+define('WORD_ENTRY_ORIGINAL', 'Palabra/frase original:');
+define('WORD_ENTRY_REPLACEMENT', 'Palabra/frase de reemplazo:');
+define('KEYWORD_TITLE', 'Palabras clave');
+define('KEYWORD_TITLE2', 'Nº búsquedas');
+
+define('SIDEBAR_HEADING', 'Opciones de palabra clave');
+define('SIDEBAR_INFO_1', 'Utiliza el botón a continuación para AÑADIR todos los datos nuevos en la tabla de datos a esta página.');
+define('SIDEBAR_INFO_2', 'Para eliminar <b>TODOS</b> los datos de palabra clave de la base de datos, pulsa el botón a continuación:');
+define('SIDEBAR_INFO_3', '<b>Palabras/frases de reemplazo</b><br><br>Pulsa aquí para ver/editar la información acerca de las palabras/frases mal escritas para reemplazos en búsquedas:');
+
+define('BUTTON_VIEW_WORD_LIST', 'Ver lista de palabras/frases');
+define('BUTTON_UPDATE_WORD_LIST', 'Actualizar con datos sin procesar');
+define('BUTTON_SORT_NAME', 'Ordenar por nombre');
+define('BUTTON_SORT_TOTAL', 'Ordenar por total');
+define('BUTTON_DELETE', 'Eliminar');
+define('BUTTON_EDIT_WORD', 'Editar esta palabra');
+define('BUTTON_ADD_WORD', 'Añadir palabra/frase a la tabla');
+
+define('LINK_EDIT', 'Editar');
+define('LINK_DELETE', 'Eliminar');
+
+define('TEXT_NO_RESULTS', 'En estos momentos no existen términos de busqueda que mostrar.');
+?>
