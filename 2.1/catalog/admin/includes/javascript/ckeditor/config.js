8c3b704840d4a3f6595f58165ac01e1f148aa5cc
michael.oscmax@gmail.com <michael.oscmax@gmail.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2011-12-23 18:00:34 +0000

    Feature #404 - integrated file manager http://bugtrack.oscmax.com/view.php?id=404

--- 2.1/catalog/admin/includes/javascript/ckeditor/config.js
+++ 2.1/catalog/admin/includes/javascript/ckeditor/config.js
@@ -3,9 +3,15 @@ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.editorConfig = function( config )
-{
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function( config ) {
+
+config.filebrowserBrowseUrl = './includes/javascript/ckeditor/filemanager/index.php?editor=ckeditor';
+config.filebrowserImageBrowseUrl = './includes/javascript/ckeditor/filemanager/index.php?editor=ckeditor&filter=image';
+config.filebrowserFlashBrowseUrl = './includes/javascript/ckeditor/filemanager/index.php?editor=ckeditor&filter=flash';
+//define the tool bar bellow (optional)
+/*config.toolbar_Full =
+[
+['Bold', 'Table', 'Format' ,'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','-', 'Image', 'Style', '-', 'Source']
+];
+*/
 };
