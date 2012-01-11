6885ed8dfae2659d643646ae0f6a0f64ad5c2b4c
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-01-11 19:53:28 +0000

    Bug Fix: #1070 - Fixes display issue with SPPC in product_listings.
    - Also removes the extra queries creating the product price from the product listing pages as this is done in the priceformatter class.

--- 2.1/catalog/includes/modules/product_listing_col.php
+++ 2.1/catalog/includes/modules/product_listing_col.php
@@ -203,100 +203,6 @@ echo tep_draw_separator('pixel_trans.gif', '100%', '10');
   }
 }
 
-// get all product prices for products with the particular customer_group_id
-// however not necessary for customer_group_id = 0
-  if ($customer_group_id != '0') {
-// BOF QPBPP for SPPC
-    $pg_query = tep_db_query("select pg.products_id, customers_group_price as price from " . TABLE_PRODUCTS_GROUPS . " pg where (".$select_list_of_prdct_ids.") and pg.customers_group_id = '" . $customer_group_id . "' and customers_group_price != null");
-// EOF QPBPP for SPPC
-//  $no_of_pg_products = tep_db_num_rows($pg_query) ;
-    while ($pg_array = tep_db_fetch_array($pg_query)) {
-      $new_prices[] = array ('products_id' => $pg_array['products_id'], 'products_price' => $pg_array['price'], 'specials_new_products_price' => '', 'final_price' => $pg_array['price']);
-    }
-    for ($x = 0; $x < $no_of_listings; $x++) {
-// replace products prices with those from customers_group table
-      if (!empty($new_prices)) {
-        for ($i = 0; $i < count($new_prices); $i++) {
-          if ($listing[$x]['products_id'] == $new_prices[$i]['products_id']) {
-            $listing[$x]['products_price'] = $new_prices[$i]['products_price'];
-            $listing[$x]['specials_new_products_price'] = $new_prices[$i]['specials_new_products_price'];
-            $listing[$x]['final_price'] = $new_prices[$i]['final_price'];
-          }
-        }
-      } // end if(!empty($new_prices)
-      $listing[$x]['specials_new_products_price'] = ''; // makes sure that a retail specials price doesn't carry over to another customer group
-      $listing[$x]['final_price'] = $listing[$x]['products_price']; // final price should not be the retail special price
-    } // end for ($x = 0; $x < $no_of_listings; $x++)
-  } // end if ($customer_group_id != '0')
-
-// an extra query is needed for all the specials
-$specials_query = tep_db_query("select products_id, specials_new_products_price from " . TABLE_SPECIALS . " where (".$select_list_of_prdct_ids.") and status = '1' and customers_group_id = '" .$customer_group_id. "'");
-while ($specials_array = tep_db_fetch_array($specials_query)) {
-  $new_s_prices[] = array ('products_id' => $specials_array['products_id'], 'products_price' => '', 'specials_new_products_price' => $specials_array['specials_new_products_price'] , 'final_price' => $specials_array['specials_new_products_price']);
-}
-
-// add the correct specials_new_products_price and replace final_price
-for ($x = 0; $x < $no_of_listings; $x++) {
-  if (!empty($new_s_prices)) {
-    for ($i = 0; $i < count($new_s_prices); $i++) {
-      if ($listing[$x]['products_id'] == $new_s_prices[$i]['products_id']) {
-         $listing[$x]['specials_new_products_price'] = $new_s_prices[$i]['specials_new_products_price'];
-         $listing[$x]['final_price'] = $new_s_prices[$i]['final_price'];
-      }
-    }
-  } // end if(!empty($new_s_prices)
-} // end for ($x = 0; $x < $no_of_listings; $x++)
-
-// BOF QPBPP for SPPC
-    $price_breaks_query = tep_db_query("select products_id, products_price, products_qty from  " . TABLE_PRODUCTS_PRICE_BREAK . " where products_id in (" . implode(',', $list_of_prdct_ids) . ") and customers_group_id = '" . $customer_group_id . "' order by products_id, products_qty");
-    while ($price_break = tep_db_fetch_array($price_breaks_query)) {
-          $price_breaks_array[$price_break['products_id']][] = array('products_price' => $price_break['products_price'], 'products_qty' => $price_break['products_qty']);
-    }
-// get discount category plus quantity blocks and minimum order quantity for retail
-    $discount_category_query = tep_db_query("select p.products_id, p.products_qty_blocks as qtyBlocks, p.products_min_order_qty, p.products_quantity, p.manufacturers_id, p.products_weight, discount_categories_id from " . TABLE_PRODUCTS ." p left join (select products_id, discount_categories_id from " . TABLE_PRODUCTS_TO_DISCOUNT_CATEGORIES . " where products_id in (" . implode(',', $list_of_prdct_ids) . ") and customers_group_id = '" . $customer_group_id . "') as ptdc on p.products_id = ptdc.products_id where p.products_id in (" . implode(',', $list_of_prdct_ids) . ")");
-      while ($dc_array = tep_db_fetch_array($discount_category_query)) {
-        $discount_categories[] = array ('products_id' => $dc_array['products_id'], 'qtyBlocks' => ($customer_group_id == '0' ? $dc_array['qtyBlocks']: '1'), 'products_min_order_qty' => ($customer_group_id == '0' ? $dc_array['products_min_order_qty']: '1'), 'products_quantity' => $dc_array['products_quantity'], 'products_weight' => $dc_array['products_weight'], 'discount_categories_id' => $dc_array['discount_categories_id']);
-      }
-      if(!empty($discount_categories)) {
-        $no_of_discount_cats = count($discount_categories);
-      for ($x = 0; $x < $no_of_listings; $x++) {
-// add discount categories to the listing array 
-        for ($i = 0; $i < $no_of_discount_cats; $i++) {
-            if ($listing[$x]['products_id'] == $discount_categories[$i]['products_id'] ) {
-                $listing[$x]['discount_categories_id'] = $discount_categories[$i]['discount_categories_id'];
-                $listing[$x]['qtyBlocks'] = $discount_categories[$i]['qtyBlocks'];
-                $listing[$x]['products_min_order_qty'] = $discount_categories[$i]['products_min_order_qty'];
-                $listing[$x]['products_quantity'] = $discount_categories[$i]['products_quantity'];
-                $listing[$x]['products_weight'] = $discount_categories[$i]['products_weight'];
-              }
-          } // end for ($i = 0; $i < $no_of_discount_cats; $i++) {
-        }
-    } // end if(!empty($discount_categories)
-
-// if customer group id is not retail we will have to do another query to get the 
-// quantity blocks and minimum order quantity
-  if ($customer_group_id != '0') {
-    $pg_qb_moq_query = tep_db_query("select pg.products_id, pg.products_qty_blocks as qtyBlocks, pg.products_min_order_qty from " . TABLE_PRODUCTS_GROUPS . " pg where products_id in (" . implode(',', $list_of_prdct_ids) . ") and pg.customers_group_id = '" . $customer_group_id . "'");
-
-      while ($pg_qb_moq_array = tep_db_fetch_array($pg_qb_moq_query)) {
-       $new_qb_moq[] = array ('products_id' => $pg_qb_moq_array['products_id'], 'qtyBlocks' => $pg_qb_moq_array['qtyBlocks'], 'products_min_order_qty' => $pg_qb_moq_array['products_min_order_qty']);
-      }
-      if (!empty($new_qb_moq)) {
-        $no_of_pg_qb_moq = count($new_qb_moq);
-      for ($x = 0; $x < $no_of_listings; $x++) {
-        for  ($i = 0; $i < $no_of_pg_qb_moq; $i++) {
-          if ($listing[$x]['products_id'] == $new_qb_moq[$i]['products_id'] ) {
-                $listing[$x]['qtyBlocks'] = $new_qb_moq[$i]['qtyBlocks'];
-                $listing[$x]['products_min_order_qty'] = $new_qb_moq[$i]['products_min_order_qty'];
-          }
-        }
-      }
-    } // end if (!empty($new_qb_moq))
-  } // end if ($customer_group_id != '0')
-// EOF QPBPP for SPPC
-
-
-
 //    while ($listing = tep_db_fetch_array($listing_query)) { (was original code)
 // WARNING the code assumes there are three products per row. To use a different number change the number
 // at line 195: if ($column >= 3) and the code to fill up the table row below that accordingly
@@ -436,7 +342,7 @@ for ($x = 0; $x < $no_of_listings; $x++) {
             if (isset($price_breaks_array[$listing[$x]['products_id']])) {
               $price_breaks_from_listing = $price_breaks_array[$listing[$x]['products_id']];
             }
-            $pf->loadProduct($listing[$x]['products_id'], $languages_id, $listing[$x], $price_breaks_from_listing);
+            $pf->loadProduct($listing[$x]['products_id'], $languages_id, NULL, $price_breaks_from_listing);
             $lc_text = $pf->getPriceStringShort();
 // EOF QPBPP for SPPC
             break;
