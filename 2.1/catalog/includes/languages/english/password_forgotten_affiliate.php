c2518f15b3aee87e6706e83513e387196b0899df
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-01-28 18:03:34 +0000

    Bug Fix: #1082 - Small fix for page title on affiliate password forgotten page.

--- 2.1/catalog/includes/languages/english/password_forgotten_affiliate.php
+++ 2.1/catalog/includes/languages/english/password_forgotten_affiliate.php
@@ -10,12 +10,13 @@ $Id: password_forgotten.php 982 2011-01-06 02:53:12Z michael.oscmax@gmail.com $
   Released under the GNU General Public License
 */
 
+define('NAVBAR_TITLE', 'Password Forgotten');
 define('NAVBAR_TITLE_1', 'Login');
 define('NAVBAR_TITLE_2', 'Password Forgotten');
 
 define('HEADING_TITLE', 'I\'ve Forgotten My Password!');
 
-define('TEXT_MAIN', 'If you\'ve forgotten your password, enter your e-mail address below and we\'ll send you an e-mail message containing your new password.');
+define('TEXT_MAIN', 'If you\'ve forgotten your affiliate password, enter your e-mail address below and we\'ll send you an e-mail message containing your new password.');
 
 define('TEXT_NO_EMAIL_ADDRESS_FOUND', 'Error: The E-Mail Address was not found in our records, please try again.');
 
