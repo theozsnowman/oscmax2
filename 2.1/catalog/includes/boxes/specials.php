af6efa1ae8325602fd81047bfe9b0b242d9f752b
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-02-18 23:30:59 +0000

    Tweak: Removes specials infobox supression which only displays the box when at product level.

--- 2.1/catalog/includes/boxes/specials.php
+++ 2.1/catalog/includes/boxes/specials.php
@@ -12,7 +12,7 @@ $Id$
 
 // Most of this file is changed or moved to BTS - Basic Template System - format.
 
-  if (isset($_GET['products_id'])){
+//  if (isset($_GET['products_id'])){
   	
 // BOF Separate Price per Customer
 /*  if ($random_product = tep_random_select("select p.products_id, pd.products_name, p.products_price, p.products_tax_class_id, p.products_image, s.specials_new_products_price from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_SPECIALS . " s where p.products_status = '1' and p.products_id = s.products_id and pd.products_id = s.products_id and pd.language_id = '" . (int)$languages_id . "' and s.status = '1' order by s.specials_date_added desc limit " . MAX_RANDOM_SELECT_SPECIALS)) { */
@@ -60,5 +60,5 @@ include (bts_select('boxes', $box_base_name)); // BTS 1.5
 <!-- specials_eof //-->
 <?php
   }
- } 
+// } 
 ?>
