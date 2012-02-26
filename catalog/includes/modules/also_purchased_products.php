564a7253bad73a25d45863c79bc8ac80b22fc3c1
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-02-26 14:35:25 +0000

    Bug Fix: #1123 - Changes "Also Purchased" module to use PriceFormatter class.

--- catalog/includes/modules/also_purchased_products.php
+++ catalog/includes/modules/also_purchased_products.php
@@ -11,7 +11,7 @@ Released under the GNU General Public License
 */ 
 
 if ($_GET['products_id']) { 
-  $ap_query = tep_db_query("select distinct p.products_id, p.products_image, pd.products_name, p.products_tax_class_id, p.products_price from " . TABLE_ORDERS_PRODUCTS . " opa, " . TABLE_ORDERS_PRODUCTS . " opb, " . TABLE_ORDERS . " o, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd left join " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c using(products_id) left join " . TABLE_CATEGORIES . " c using(categories_id) where opa.products_id = '" . (int)$_GET['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . (int)$_GET['products_id'] . "' and opb.products_id = p.products_id and opb.orders_id = o.orders_id and p.products_id = pd.products_id and p.products_status = '1' and find_in_set('".$customer_group_id."', p.products_hide_from_groups) = '0' group by p.products_id order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
+  $ap_query = tep_db_query("select distinct p.products_id, p.products_image, pd.products_name from " . TABLE_ORDERS_PRODUCTS . " opa, " . TABLE_ORDERS_PRODUCTS . " opb, " . TABLE_ORDERS . " o, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd left join " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c using(products_id) left join " . TABLE_CATEGORIES . " c using(categories_id) where opa.products_id = '" . (int)$_GET['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . (int)$_GET['products_id'] . "' and opb.products_id = p.products_id and opb.orders_id = o.orders_id and p.products_id = pd.products_id and p.products_status = '1' and find_in_set('" . $customer_group_id . "', p.products_hide_from_groups) = '0' group by p.products_id order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
 $num_products_ap = tep_db_num_rows($ap_query); 
 if ($num_products_ap != 0) { // Check query is not blank
 
@@ -22,14 +22,6 @@ if ($num_products_ap != 0) { // Check query is not blank
 	  $div3check_ap = $div3_ap - $div3main_ap;
 	  $div3rows_ap = $div3_ap - $div3check_ap;
   // PGM END
-  
-  // BOF Separate Price per Customer
-          if(!tep_session_is_registered('sppc_customer_group_id')) { 
-            $customer_group_id = '0';
-          } else {
-            $customer_group_id = $sppc_customer_group_id;
-          }
-  // EOF Separate Price per Customer
   
     if ($num_products_ap >= MIN_DISPLAY_ALSO_PURCHASED) { 
 
@@ -69,27 +61,16 @@ if ($num_products_ap != 0) { // Check query is not blank
       $col = 0;
 
       while ($ap = tep_db_fetch_array($ap_query)) {
-		
-        $ap['specials_new_products_price'] = tep_get_products_special_price($ap['products_id']);
-		  if ($ap['specials_new_products_price']) { 
-            $ap_price =  '<span style="text-decoration:line-through">' . $currencies->display_price($ap['products_price'], tep_get_tax_rate($ap['products_tax_class_id'])) . '</span>&nbsp;&nbsp;'; 
-            $ap_price .= '<span class="productSpecialPrice">' . $currencies->display_price($ap['specials_new_products_price'], tep_get_tax_rate($ap['products_tax_class_id'])) . '</span>'; 
-          } else {
-			// BOF Separate Price per Customer  
-            $customer_group_price_query = tep_db_query("select customers_group_price from " . TABLE_PRODUCTS_GROUPS . " where products_id = '" . (int)$ap['products_id']. "' and customers_group_id =  '" . $customer_group_id . "'");
-              if ($customer_group_price = tep_db_fetch_array($customer_group_price_query)) {
-                $ap_price = $currencies->display_price($customer_group_price['customers_group_price'], tep_get_tax_rate($ap['products_tax_class_id']));
-	          } else {  
-            // EOF Separate Price per Customer
-                $ap_price =  $currencies->display_price($ap['products_price'], tep_get_tax_rate($ap['products_tax_class_id'])); 
-			  }
-	      }
-		  
+ 
+        $pf->loadProduct($ap['products_id'], $languages_id, NULL, NULL);
+				  
 		  if (SHOW_MORE_INFO == 'True') {
             $more_info = '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . tep_image_button('button_more_info.gif', IMAGE_BUTTON_MORE_INFO) . '</a>';
+		  } else {
+			$more_info = '';
 		  }
 
-		  $display_code_ap = '<td width="33%" class="smallText"><br><center><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . tep_image(DIR_WS_IMAGES . DYNAMIC_MOPICS_THUMBS_DIR . $ap['products_image'], $ap['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . $ap['products_name'] .'</a><br>' . $ap_price . '<br>' . $more_info . ' <a href="' . tep_href_link( FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id'] .  '&amp;action=buy_now&product_to_buy_id=' . $ap['products_id'], 'NONSSL') . '">' . tep_image_button('button_buy_now.gif', '' . $ap['products_name'] . '') .'</a></center></td>';
+		  $display_code_ap = '<td width="33%" class="smallText"><br><center><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . tep_image(DIR_WS_IMAGES . DYNAMIC_MOPICS_THUMBS_DIR . $ap['products_image'], $ap['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . $ap['products_name'] .'</a><br>' . $pf->getPriceStringShort() . '<br>' . $more_info . ' <a href="' . tep_href_link( FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id'] .  '&amp;action=buy_now&product_to_buy_id=' . $ap['products_id'], 'NONSSL') . '">' . tep_image_button('button_buy_now.gif', '' . $ap['products_name'] . '') .'</a></center></td>';
  
 	  if  (($div3rows_ap <> $row) && ($col == 0)) {
 		$output_ap .= '<div><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr valign="middle">' . $display_code_ap;
@@ -157,31 +138,20 @@ if ($num_products_ap != 0) { // Check query is not blank
       $row = 0;
       $col = 0;
       $info_box_contents = array();
-      while ($ap = tep_db_fetch_array($ap_query)) {
-		
-        $ap['specials_new_products_price'] = tep_get_products_special_price($ap['products_id']); 
-
-		if ($ap['specials_new_products_price']) { 
-      	  $ap_price =  '<span style="text-decoration:line-through">' . $currencies->display_price($ap['products_price'], tep_get_tax_rate($ap['products_tax_class_id'])) . '</span>&nbsp;&nbsp;'; 
-      	  $ap_price .= '<span class="productSpecialPrice">' . $currencies->display_price($ap['specials_new_products_price'], tep_get_tax_rate($ap['products_tax_class_id'])) . '</span>'; 
-    	} else { 
-		  // BOF Separate Price per Customer  
-          $customer_group_price_query = tep_db_query("select customers_group_price from " . TABLE_PRODUCTS_GROUPS . " where products_id = '" . (int)$ap['products_id']. "' and customers_group_id =  '" . $customer_group_id . "'");
-          if ($customer_group_price = tep_db_fetch_array($customer_group_price_query)) {
-            $ap_price = $currencies->display_price($customer_group_price['customers_group_price'], tep_get_tax_rate($ap['products_tax_class_id']));
-	      } else {  
-          // EOF Separate Price per Customer
-	        $ap_price =  $currencies->display_price($ap['products_price'], tep_get_tax_rate($ap['products_tax_class_id'])); 
-		  }
-	    } 
+	  
+	  while ($ap = tep_db_fetch_array($ap_query)) {
+	  
+        $pf->loadProduct($ap['products_id'], $languages_id, NULL, NULL); 
 		
 		if (SHOW_MORE_INFO == 'True') {
           $more_info = '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . tep_image_button('button_more_info.gif', IMAGE_BUTTON_MORE_INFO) . '</a>';
+		} else {
+			$more_info = '';
 		}
      
 	  $info_box_contents[$row][$col] = array('align' => 'center',
                                              'params' => 'class="smallText" width="33%" valign="top"',
-                                             'text' => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . tep_image(DIR_WS_IMAGES . DYNAMIC_MOPICS_THUMBS_DIR . $ap['products_image'], $ap['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . $ap['products_name'] .'</a><br>' . $ap_price . '<br>' .$more_info . ' <a href="' . tep_href_link( FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id'] .  '&amp;action=buy_now&product_to_buy_id=' . $ap['products_id'], 'NONSSL') . '">' . tep_image_button('button_buy_now.gif', '' . $ap['products_name'] . '') .'</a>');
+                                             'text' => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . tep_image(DIR_WS_IMAGES . DYNAMIC_MOPICS_THUMBS_DIR . $ap['products_image'], $ap['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id']) . '">' . $ap['products_name'] .'</a><br>' . $pf->getPriceStringShort() . '<br>' .$more_info . ' <a href="' . tep_href_link( FILENAME_PRODUCT_INFO, 'products_id=' . $ap['products_id'] .  '&amp;action=buy_now&product_to_buy_id=' . $ap['products_id'], 'NONSSL') . '">' . tep_image_button('button_buy_now.gif', '' . $ap['products_name'] . '') .'</a>');
       $col ++;
         if ($col > 2) {
           $col = 0;
