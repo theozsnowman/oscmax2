ce73fc0dd53de98f29a04abf9ebf8076a6bab57c
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-03-13 21:19:07 +0000

    Bug Fix: #1150 - Fix to single product image regen - thanks to SMurphy for code fix

--- catalog/admin/includes/languages/english/core.php
+++ catalog/admin/includes/languages/english/core.php
@@ -505,6 +505,7 @@ define('IMAGE_SUMMARY', 'Summary');
 define('IMAGE_BROWSE', 'Browse');
 define('IMAGE_MISSING', 'Missing');
 define('IMAGE_ORPHANS', 'Orphans');
+define('IMAGE_REGENERATE', 'Regenerate');
 define('IMAGE_REGENERATE_ALL', 'Regenerate all images for this product');
 define('IMAGE_REGENERATE_EVERYTHING', 'Regenerate all your images');
 define('IMAGE_MC_SYNC', 'Sync with MailChimp');
