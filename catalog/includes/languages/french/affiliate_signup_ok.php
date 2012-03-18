b6633951baaf25d88deb7cd9b2cd3dafd63ebe24
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-03-18 14:33:35 +0000

    Bug Fix: #1087 - Language file fixes for affiliate_signup_ok.

--- catalog/includes/languages/french/affiliate_signup_ok.php
+++ catalog/includes/languages/french/affiliate_signup_ok.php
@@ -12,5 +12,6 @@ $Id$
 
 define('NAVBAR_TITLE', 'Affiliation - Enregistrement');
 define('HEADING_TITLE', 'Félicitations !');
-define('TEXT_ACCOUNT_CREATED', 'Félicitations ! Votre nouveau compte affilié a bien été envoyé ! Vous allez rapidement recevoir un e-mail contenant les informations importantes concernant votre compte affilié.<br>Si vous n\'avez rien reçu d\'ici une heure, veuillez <a href="' . tep_href_link(FILENAME_AFFILIATE_CONTACT) . '">nous contacter</a>.<br><br>Si vous avez <small><b>n\'importe quelles</b></small> questions à propos du programme d\'affiliation, n\'hésitez pas à <a href="' . tep_href_link(FILENAME_AFFILIATE_CONTACT) . '">nous contacter</a>.');
+define('TEXT_ACCOUNT_CREATED', 'Félicitations ! Votre nouveau compte affilié a bien été envoyé ! Vous allez rapidement recevoir un e-mail contenant les informations importantes concernant votre compte affilié.<br>Si vous n\'avez rien reçu d\'ici une heure, veuillez ');
+define('TEXT_CONTACT_US', 'nous contacter');
 ?>
