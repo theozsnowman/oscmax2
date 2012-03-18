657351d4d1a7637732e1f97687978a8af8c94a4c
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-03-18 14:43:05 +0000

    Bug Fix: #1087 - Language file fixes for affiliate_signup.

--- catalog/includes/languages/german/affiliate_signup.php
+++ catalog/includes/languages/german/affiliate_signup.php
@@ -13,14 +13,14 @@ $Id$
 define('NAVBAR_TITLE', 'Affiliateprogramm');
 define('HEADING_TITLE', 'Anmeldung zu unserem Affiliateprogramm');
 
-define('MAIL_AFFILIATE_SUBJECT', 'Willkommen zum Affiliateprogramm von' . STORE_NAME);
+define('MAIL_AFFILIATE_SUBJECT', 'Willkommen zum Affiliateprogramm von %s');
 define('MAIL_GREET_NONE', 'Hallo %s' . "\n\n");//em001
-define('MAIL_AFFILIATE_HEADER', 'Wir freuen uns, dass Sie am Werbepartnerpogramm von<b>' . STORE_NAME . '</b>teilnehmen.' . "\n\n" . 'Ihre Anmeldeinformationen:
+define('MAIL_AFFILIATE_HEADER', 'Wir freuen uns, dass Sie am Werbepartnerpogramm von<b>%s</b>teilnehmen.' . "\n\n" . 'Ihre Anmeldeinformationen:
 **********************************************'."\n\n");
 define('MAIL_AFFILIATE_ID', 'Ihre Partner-ID lautet: %s' . "\n");
 define('MAIL_AFFILIATE_USERNAME', 'Ihr Benutzername lautet: %s' . "\n");
 define('MAIL_AFFILIATE_PASSWORD', 'Ihr Passwort lautet: %s' . "\n");
 define('MAIL_AFFILIATE_LINK', 'Melden Sie sich hier an: %s');
-define('MAIL_AFFILIATE_FOOTER', 'Wir wünschen Ihnen viel Erfolg!'."\n\n".'Ihr <b>' . STORE_NAME . '</b> Affiliate Team' . "\n" . AFFILIATE_EMAIL_ADDRESS);
+define('MAIL_AFFILIATE_FOOTER', 'Wir wünschen Ihnen viel Erfolg!'."\n\n".'Ihr <b>%s</b> Affiliate Team' . "\n" . '%s');
 define('IMAGE_BUTTON_MAT', 'Bitte akzeptieren Sie unsere Affiliate AGB');
 ?>
