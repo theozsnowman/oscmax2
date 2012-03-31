feba9f70df26ca7ac36640074fbe7c65f5d14ccc
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-03-31 07:59:46 +0000

    Bug Fix: #1145 - Small change to product_listing to remove hard coded HTML styling

--- catalog/includes/modules/product_listing.php
+++ catalog/includes/modules/product_listing.php
@@ -399,9 +399,9 @@ echo tep_draw_separator('pixel_trans.gif', '100%', '10');
             // end extra product fields
             $lc_align = '';
             if (isset($_GET['manufacturers_id'])) {
-              $lc_text = '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'manufacturers_id=' . $_GET['manufacturers_id'] . '&products_id=' . $listing[$x]['products_id']) . '"><b>' . $listing[$x]['products_name'] . '</b>' . $short /*begin epf*/ . $extra /*end epf*/ . '</a>';
+              $lc_text = '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'manufacturers_id=' . $_GET['manufacturers_id'] . '&products_id=' . $listing[$x]['products_id']) . '">' . $listing[$x]['products_name'] . $short /*begin epf*/ . $extra /*end epf*/ . '</a>';
             } else {
-              $lc_text = '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, ($cPath ? 'cPath=' . $cPath . '&' : '') . 'products_id=' . $listing[$x]['products_id']) . '"><b>' . $listing[$x]['products_name'] . '</b>' . $short /*begin epf*/ . $extra /*end epf*/ . '</a>&nbsp;';
+              $lc_text = '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, ($cPath ? 'cPath=' . $cPath . '&' : '') . 'products_id=' . $listing[$x]['products_id']) . '">' . $listing[$x]['products_name'] . $short /*begin epf*/ . $extra /*end epf*/ . '</a>&nbsp;';
             }
             break;
           case 'PRODUCT_LIST_MANUFACTURER':
