cc86a930729f37b6aa4e1f7f74ea0dcb10c47349
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-04-09 18:24:40 +0000

    Bug Fix: #1087 - Update to remove code from language defines.

--- catalog/includes/languages/espanol/catalog_products_with_images.php
+++ catalog/includes/languages/espanol/catalog_products_with_images.php
@@ -11,10 +11,8 @@ $Id$
 */
 
 define('TOP_BAR_TITLE', 'Catálogo imprimible');
-//define('HEADING_TITLE', '<table border="0" width="100%" cellspacing="3" cellpadding="3"><tr><td width="30%" class="pageHeading"><h5>' . STORE_NAME_ADDRESS . '</h5></td><td width="70%"><h6><u>Nota</U>:<br> Todos los precios están sujetos a cambio sin previo aviso.  Todos los productos serán facturados en moneda canadiense sólamente.  La funcionalidad de moneda está simplemente por su comodidad.  Este catálogo está actualizado al día de la impresión.</h6></td></tr></table>');
-define('HEADING_TITLE', '<h6>'.STORE_NAME.'<br>'.nl2br(STORE_NAME_ADDRESS).'<br>Email: <a href="mailto:'.STORE_OWNER_EMAIL_ADDRESS.'">'.STORE_OWNER_EMAIL_ADDRESS.'</a><br>Nota: Todos los precios están sujetos a cambio sin previo aviso. Todos los productos serán facturados con la moneda de nuestra tienda sólamente.<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;La funcionalidad de moneda está simplemente por su comodidad.  Este catálogo está actualizado al día de la impresión.</h6>');
-// comment the above line and uncomment out the line above it if you have an older OSC Release before Nov1 2002
-// comenta la línea anterior y quita el comentario de la línea sobre ella si tienes una versión de OSC de antes de Nov1 2002
+define('TEXT_EMAIL', 'Email: ');
+define('TEXT_SUBJECT_TO_CHANGE', '>Nota: Todos los precios están sujetos a cambio sin previo aviso. Todos los productos serán facturados con la moneda de nuestra tienda sólamente. Este catálogo está actualizado al día de la impresión.');
 define('TABLE_HEADING_IMAGES', 'Imagen');
 define('TABLE_HEADING_MANUFACTURERS', 'Fabricante');
 define('TABLE_HEADING_PRODUCTS', 'Nombre');
--- catalog/includes/languages/french/catalog_products_with_images.php
+++ catalog/includes/languages/french/catalog_products_with_images.php
@@ -10,9 +10,8 @@ $Id$
   Released under the GNU General Public License
 */
 define('TOP_BAR_TITLE', 'Catalog Imprimable');
-//define('HEADING_TITLE', '<table border="0" width="100%" cellspacing="3" cellpadding="3"><tr><td width="30%" class="pageHeading"><h5>' . STORE_NAME_ADDRESS . '</h5></td><td width="70%"><h6><u>Note</U>:<br> All prices are subject to change without notice. All products will be billed in Canadian Currency only. The Currency feature is just for your convenience. This catalog is current on the day of printing.</h6></td></tr></table>');
-define('HEADING_TITLE', '<h6>'.STORE_NAME.'<br>'.nl2br(STORE_NAME_ADDRESS).'<br>Email: <a href="mailto:'.STORE_OWNER_EMAIL_ADDRESS.'">'.STORE_OWNER_EMAIL_ADDRESS.'</a><br>Important: Tous les prix indiqués peuvent varier selon les conditions économiques à la commande. Tous les produits sont affichés dans notre devise € .<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Les prix indiqués correspondent à ceux de notre site web. Ils sont automatiquement mis à jour à chauqe fois que vous générez le catalogue imprimable.</h6>');
-// comment the above line and uncomment out the line above it if you have an older OSC Release before Nov1 2002
+define('TEXT_EMAIL', 'Email: ');
+define('TEXT_SUBJECT_TO_CHANGE', 'Important: Tous les prix indiqués peuvent varier selon les conditions économiques à la commande. Tous les produits sont affichés dans notre devise €. Ils sont automatiquement mis à jour à chauqe fois que vous générez le catalogue imprimable. Les prix indiqués correspondent à ceux de notre site web. Ils sont automatiquement mis à jour à chauqe fois que vous générez le catalogue imprimable.');
 define('TABLE_HEADING_IMAGES', 'Image');
 define('TABLE_HEADING_MANUFACTURERS', 'Constructeur');
 define('TABLE_HEADING_PRODUCTS', 'Nom');
