e635b18c3ad84526607633b8b5301c9028061adc
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-04-14 09:35:16 +0000

    Bug Fix: #1087 - Moves php out of language files for wishlist_help.php

--- catalog/includes/languages/german/wishlist_help.php
+++ catalog/includes/languages/german/wishlist_help.php
@@ -17,39 +17,34 @@ define('HEADING_TITLE', 'Wunschliste - Häufig gestellte Fragen');
 
 define('TEXT_CLOSE_WINDOW', '<u>Fenster schließen</u> [x]');
 
-define('TEXT_INFORMATION', '<b>Was ist die ' . BOX_HEADING_CUSTOMER_WISHLIST . '?</b><br>' .
-BOX_HEADING_CUSTOMER_WISHLIST . ' ist eine komfortable Möglichkeit, Produkte vorzumerken, die Sie später kaufen wollen oder gerade nicht auf Lager sind.
+define('TEXT_INFORMATION', '<b>Was ist die Wunschliste?</b><br>Wunschliste ist eine komfortable Möglichkeit, Produkte vorzumerken, die Sie später kaufen wollen oder gerade nicht auf Lager sind.
 <br>
 <br>
-<b>Wie füge ich Produkte zur ' . BOX_HEADING_CUSTOMER_WISHLIST . ' hinzu? </b><br>
-Um ein Produkt der ' . BOX_HEADING_CUSTOMER_WISHLIST . ' hinzuzufügen, klicken Sie einfach bei jedem Produkt, dass Sie interessiert, auf "Zur Wunschliste hinzufügen". Der Button erscheint neben dem "In den Warenkorb" Button auf der jeweiligen Produktseite.
+<b>Wie füge ich Produkte zur Wunschliste hinzu? </b><br>
+Um ein Produkt der Wunschliste hinzuzufügen, klicken Sie einfach bei jedem Produkt, dass Sie interessiert, auf "Zur Wunschliste hinzufügen". Der Button erscheint neben dem "In den Warenkorb" Button auf der jeweiligen Produktseite.
 <br>
 <br>
 <b>Kann ich nicht lagernde oder noch nicht verfügbare Produkte hinzufügen?</b><br>
-Ja. Sie können jedes Produkt der ' . BOX_HEADING_CUSTOMER_WISHLIST . ' hinzufügen.' .	
+Ja. Sie können jedes Produkt der Wunschliste hinzufügen.' .	
 '<br>
 <br>
-<b>Wie kann ich mir die  ' . BOX_HEADING_CUSTOMER_WISHLIST . ' anzeigen lassen?</b><br>
-In der Spalte rechts befindet sich eine "' . BOX_HEADING_CUSTOMER_WISHLIST . '"-Box, in der der aktuelle Inhalt der ' . BOX_HEADING_CUSTOMER_WISHLIST . ' angezeigt wird. Wenn sich 5 oder weniger Artikel in der ' . BOX_HEADING_CUSTOMER_WISHLIST . ', befinden, werden sie direkt aufgelistet. Bei mehr als fünf Produkten wird nur die Anzahl der Produkte angezeigt.
+<b>Wie kann ich mir die  Wunschliste anzeigen lassen?</b><br>
+In der Spalte rechts befindet sich eine "Wunschliste"-Box, in der der aktuelle Inhalt der Wunschliste angezeigt wird. Wenn sich 5 oder weniger Artikel in der Wunschliste, befinden, werden sie direkt aufgelistet. Bei mehr als fünf Produkten wird nur die Anzahl der Produkte angezeigt.
 <br>
 <br>
-Die ' . BOX_HEADING_CUSTOMER_WISHLIST . ' können Sie jederzeit durch einen Klick auf den Link in der "' . BOX_HEADING_CUSTOMER_WISHLIST . '"-Box (<i><u>' . BOX_HEADING_CUSTOMER_WISHLIST . ' anzeigen</u> [+]</i>).  oder durch Klick auf den Pfeil auf der "' . BOX_HEADING_CUSTOMER_WISHLIST . '"-Box.
+Die Wunschliste können Sie jederzeit durch einen Klick auf den Link in der "Wunschliste"-Box (<i><u>Wunschliste anzeigen</u> [+]</i>).  oder durch Klick auf den Pfeil auf der "Wunschliste"-Box.
 <br>
 <br>
-<b>Wie kann ich Produkte von der ' . BOX_HEADING_CUSTOMER_WISHLIST . ' in meinen Warenkorb verschieben?</b><br>
-Um Produkte von der ' . BOX_HEADING_CUSTOMER_WISHLIST . ' in den Warenkorb zu verschieben, klicken Sie entweder unter dem Produktnamen auf "In den Warenkorb verschieben" in der "' . BOX_HEADING_CUSTOMER_WISHLIST . '"-Box oder klicken Sie auf der Wunschlistenseite auf "In den Warenkorb" unter dem Produkt.
+<b>Wie kann ich Produkte von der Wunschliste in meinen Warenkorb verschieben?</b><br>
+Um Produkte von der Wunschliste in den Warenkorb zu verschieben, klicken Sie entweder unter dem Produktnamen auf "In den Warenkorb verschieben" in der "Wunschliste"-Box oder klicken Sie auf der Wunschlistenseite auf "In den Warenkorb" unter dem Produkt.
 <br>
 <br>
-<b>Wie lösche ich Produkte von der ' . BOX_HEADING_CUSTOMER_WISHLIST . '?</b><br>
-Um Produkte von der  ' . BOX_HEADING_CUSTOMER_WISHLIST . ' zu löschen, klicken Sie entweder auf "Löschen" unter dem Produkt in der "' . BOX_HEADING_CUSTOMER_WISHLIST . '"-Box oder klicken Sie "Löschen" unter dem Produkt in der ' . BOX_HEADING_CUSTOMER_WISHLIST . '-Hauptseite. 
+<b>Wie lösche ich Produkte von der Wunschliste?</b><br>
+Um Produkte von der  Wunschliste zu löschen, klicken Sie entweder auf "Löschen" unter dem Produkt in der "Wunschliste"-Box oder klicken Sie "Löschen" unter dem Produkt in der Wunschliste-Hauptseite. 
 <br>
 <br>
-<!-- BoF Modification by: Talon177 -->
-<b> Kann ich meine ' . BOX_HEADING_CUSTOMER_WISHLIST . ' Anderen zukommen lassen? </b><br>
-<!-- EoF Modification by: Talon177 -->
-<!-- BoF Modification by: Talon177 & Rob Woodgate -->
-Derzeit leider nicht. Ihre ' . BOX_HEADING_CUSTOMER_WISHLIST . ' ist nur verfügbar, wenn Sie angemeldet sind. Daher kann Therefore, you are the only one that can see it.  Sie können aber Ihre ' . BOX_HEADING_CUSTOMER_WISHLIST . ' versenden, indem Sie einen oder mehrere Empfänger in auf der Wunschlistenseite bestimmen.
-<!-- EoF Modification by: Talon177 & Rob Woodgate-->
+<b> Kann ich meine Wunschliste Anderen zukommen lassen? </b><br>
+Derzeit leider nicht. Ihre Wunschliste ist nur verfügbar, wenn Sie angemeldet sind. Daher kann Therefore, you are the only one that can see it.  Sie können aber Ihre Wunschliste versenden, indem Sie einen oder mehrere Empfänger in auf der Wunschlistenseite bestimmen.
 <br>
 <br>');
 ?>
