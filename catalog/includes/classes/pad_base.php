b1cd668cb4bbdabfc48013ae25a2a85c5d472b70
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2012-11-24 03:25:14 +0000

    Bug Fix: #1256 - Pad_base fix by SMurphy

--- catalog/includes/classes/pad_base.php
+++ catalog/includes/classes/pad_base.php
@@ -420,10 +420,10 @@ $products_options_array[] = array('id' => $products_options['products_options_va
     // Code to show the attribute price combined with the base cost
       if ($products_options['price_prefix'] == '+') {
         $real_price = ($hi_product_price) + ($products_options['options_values_price']);
-        $products_options_array[sizeof($products_options_array)-1]['text'] .= ' (' . $currencies->display_price($real_price, tep_get_tax_rate($product_info['products_tax_class_id'])) .') ';
+        $products_options_array[sizeof($products_options_array)-1]['text'] .= ' (' . $currencies->display_price($real_price, tep_get_tax_rate($this->products_tax_class_id)) .') ';
       } else {
         $real_price = ($hi_product_price) - ($products_options['options_values_price']);
-        $products_options_array[sizeof($products_options_array)-1]['text'] .= ' (' . $currencies->display_price($real_price, tep_get_tax_rate($product_info['products_tax_class_id'])) .') ';
+        $products_options_array[sizeof($products_options_array)-1]['text'] .= ' (' . $currencies->display_price($real_price, tep_get_tax_rate($this->products_tax_class_id)) .') ';
       } 	  
     } else {
 	// Standard method showing attribute price seperately
