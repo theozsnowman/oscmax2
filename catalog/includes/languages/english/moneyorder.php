e5c086db9a55ab46b0370c4fce37a5e49921cc0a
michael.oscmax@gmail.com <michael.oscmax@gmail.com@a4920382-5936-0410-8c60-f14f5803c0f4>
2013-02-08 20:31:35 +0000

    Bug Fix #1303 - moneyorder.php language file - formatting problem. http://bugtrack.oscmax.com/view.php?id=1303

--- catalog/includes/languages/english/moneyorder.php
+++ catalog/includes/languages/english/moneyorder.php
@@ -12,5 +12,5 @@ $Id$
 
   define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Check/Money Order');
   define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Make Payable To:&nbsp;%s<br><br>Send To:<br>%s<br><br>Your order will not ship until we receive payment.');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', 'Make Payable To: %s\n\nSend To:\n%s\n\nYour order will not ship until we receive payment.');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Make Payable To: %s\n\nSend To:\n%s\n\nYour order will not ship until we receive payment.");
 ?>
