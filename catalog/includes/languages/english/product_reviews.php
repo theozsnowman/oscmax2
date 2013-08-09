951468846a368c01e4946f9ac631ea1b51cfd701
cottonbarn <cottonbarn@a4920382-5936-0410-8c60-f14f5803c0f4>
2013-08-09 18:56:06 +0000

    Bug Fix: #1346 - Typo in product_reviews.php found by Luxoria

--- catalog/includes/languages/english/product_reviews.php
+++ catalog/includes/languages/english/product_reviews.php
@@ -16,5 +16,5 @@ define('TEXT_CLICK_TO_ENLARGE', 'Click to enlarge');
 
 define('TEXT_OF_5_STARS', '%s of 5 Stars!');
 
-define('TEXT_ARTICLE_REVIEW_SUBMITTED', 'Thank you for submitting your review of this article.  It will be published once it has been approved.');
+define('TEXT_ARTICLE_REVIEW_SUBMITTED', 'Thank you for submitting your review of this product.  It will be published once it has been approved.');
 ?>
